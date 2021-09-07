788bf13311565324bbb7f28a55d17e8842470ea3
Zabuzard <zabuza.dev@gmail.com>
2021-09-07 23:14:01 +0200

    Initial setup (!ping command) + gradle + JDA + log4j 2 with configuration + full gitignore + !ping command

---
+++ src/main/java/org/togetherjava/tjbot/PingPongListener.java
@@ -0,0 +1,33 @@
+package org.togetherjava.tjbot;
+
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class PingPongListener extends ListenerAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(PingPongListener.class);
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		if (event.getAuthor()
+				.isBot()) {
+			return;
+		}
+		if (!event.isFromType(ChannelType.TEXT)) {
+			return;
+		}
+
+		if (!"!ping".equals(event.getMessage()
+				.getContentDisplay())) {
+			return;
+		}
+
+		logger.info("#{}: Received '!ping' command", event.getResponseNumber());
+
+		event.getChannel()
+				.sendMessage("Pong!")
+				.queue();
+	}
+}
---
+++ src/test/java/org/togetherjava/tjbot/MainTest.java
@@ -0,0 +1,11 @@
+package org.togetherjava.tjbot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public final class MainTest {
+	@Test
+	void testFoo() {
+		Assertions.assertTrue(true);
+	}
+}
