6d4c8656f4b059f46d64d539767a501c0da3b317
Zabuzard <zabuza.dev@gmail.com>
2021-09-08 00:19:25 +0200

    Added ".editorconfig"
    - mostly IJ default settings, tweaked a bit.
    - spaces, CRLF, 120, wrap, strict

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 ![Java](https://img.shields.io/badge/Java-17%2B-ff696c)
 [![license](https://img.shields.io/github/license/Together-Java/TJ-Bot)](https://github.com/Together-Java/TJ-Bot/blob/master/LICENSE)
 
-TJ-Bot is a Discord Bot used on the [Together Java](https://discord.com/invite/XXFUXzK) server. It is maintained by the community, anyone can contribute.
+TJ-Bot is a Discord Bot used on the [Together Java](https://discord.com/invite/XXFUXzK) server. It is maintained by the
+community, anyone can contribute.
 
 # Requirements
 
--- src/main/java/org/togetherjava/tjbot/PingPongListener.java
+++ src/main/java/org/togetherjava/tjbot/PingPongListener.java
@@ -7,27 +7,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class PingPongListener extends ListenerAdapter {
-	private static final Logger logger = LoggerFactory.getLogger(PingPongListener.class);
+    private static final Logger logger = LoggerFactory.getLogger(PingPongListener.class);
 
-	@Override
-	public void onMessageReceived(MessageReceivedEvent event) {
-		if (event.getAuthor()
-				.isBot()) {
-			return;
-		}
-		if (!event.isFromType(ChannelType.TEXT)) {
-			return;
-		}
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor()
+                 .isBot()) {
+            return;
+        }
+        if (!event.isFromType(ChannelType.TEXT)) {
+            return;
+        }
 
-		if (!"!ping".equals(event.getMessage()
-				.getContentDisplay())) {
-			return;
-		}
+        if (!"!ping".equals(event.getMessage()
+                                 .getContentDisplay())) {
+            return;
+        }
 
-		logger.info("#{}: Received '!ping' command", event.getResponseNumber());
+        logger.info("#{}: Received '!ping' command", event.getResponseNumber());
 
-		event.getChannel()
-				.sendMessage("Pong!")
-				.queue();
-	}
+        event.getChannel()
+             .sendMessage("Pong!")
+             .queue();
+    }
 }
