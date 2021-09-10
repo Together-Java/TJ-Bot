3672527a0fe5264080a12e80fbff0285dd069122
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-09-10 20:15:37 +0200

    Migrate database listener over to jOOQ and Flyway

--- application/src/main/java/org/togetherjava/tjbot/Application.java
+++ application/src/main/java/org/togetherjava/tjbot/Application.java
@@ -4,8 +4,11 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.togetherjava.tjbot.db.Database;
 
 import javax.security.auth.login.LoginException;
+import java.nio.file.Path;
+import java.sql.SQLException;
 
 public enum Application {
     ;
@@ -15,28 +18,32 @@ public enum Application {
     /**
      * Starts the application.
      *
-     * @param args One argument - the token of the bot
+     * @param args command line arguments - [the token of the bot, the path to the database]
      */
     public static void main(final String[] args) {
-        if (args.length != 1) {
-            throw new IllegalArgumentException("Expected one argument but " + args.length
-                    + " arguments were provided. The argument must be the token of the bot.");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Expected two arguments but " + args.length
+                    + " arguments were provided. The first argument must be the token of the bot"
+                    + " and the second the path to the database.");
         }
         String token = args[0];
+        String databasePath = args[1];
 
         try {
-            runBot(token);
+            runBot(token, Path.of(databasePath));
         } catch (Exception t) {
             logger.error("Unknown error", t);
         }
     }
 
-    public static void runBot(String token) {
+    public static void runBot(String token, Path databasePath) {
         logger.info("Starting bot...");
         try {
+            Database database = new Database("jdbc:sqlite:" + databasePath.toAbsolutePath());
+
             JDA jda = JDABuilder.createDefault(token)
                                 .addEventListeners(new PingPongListener())
-                                .addEventListeners(new DatabaseListener())
+                                .addEventListeners(new DatabaseListener(database))
                                 .build();
             jda.awaitReady();
             logger.info("Bot is ready");
@@ -45,6 +52,8 @@ public enum Application {
         } catch (InterruptedException e) {
             logger.error("Interrupted while waiting for setup to complete", e);
             Thread.currentThread().interrupt();
+        } catch (SQLException e) {
+            logger.error("Failed to create database", e);
         }
     }
 }
--- application/src/main/java/org/togetherjava/tjbot/DatabaseListener.java
+++ application/src/main/java/org/togetherjava/tjbot/DatabaseListener.java
@@ -5,36 +5,20 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.togetherjava.tjbot.db.Database;
+import org.togetherjava.tjbot.db.DatabaseException;
+import org.togetherjava.tjbot.db.generated.tables.Storage;
+import org.togetherjava.tjbot.db.generated.tables.records.StorageRecord;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.sql.*;
+import java.util.Optional;
 
-public final class DatabaseListener extends ListenerAdapter implements AutoCloseable {
+public final class DatabaseListener extends ListenerAdapter {
     private static final Logger logger = LoggerFactory.getLogger(DatabaseListener.class);
-    private static final String CREATE_TABLE =
-            "CREATE TABLE IF NOT EXISTS storage (key TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL)";
 
-    private final Connection connection = connectDatabase();
+    private final Database database;
 
-    private static final String PUT_KEY_VALUE =
-            "INSERT OR REPLACE INTO storage(key, value) VALUES (?, ?)";
-    private static final String GET_VALUE = "SELECT value FROM storage WHERE key = ? LIMIT 1";
-
-    private static Connection connectDatabase() {
-        try {
-            Files.createDirectories(Path.of("db"));
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:db/database.db");
-
-            try (Statement statement = connection.createStatement()) {
-                statement.executeUpdate(CREATE_TABLE);
-            }
-
-            return connection;
-        } catch (SQLException | IOException e) {
-            throw new IllegalStateException(e);
-        }
+    public DatabaseListener(Database database) {
+        this.database = database;
     }
 
     @Override
@@ -67,13 +51,16 @@ public final class DatabaseListener extends ListenerAdapter implements AutoClose
         String key = data[1];
         String value = data[2];
 
-        try (PreparedStatement putKeyValue = connection.prepareStatement(PUT_KEY_VALUE)) {
-            putKeyValue.setString(1, key);
-            putKeyValue.setString(2, value);
-            putKeyValue.executeUpdate();
+        try {
+            database.writeTransaction(ctx -> {
+                StorageRecord record = ctx.newRecord(Storage.STORAGE).setKey(key).setValue(value);
+                if (record.update() == 0) {
+                    record.insert();
+                }
+            });
 
             event.getChannel().sendMessage("Saved under '" + key + "'.").queue();
-        } catch (SQLException e) {
+        } catch (DatabaseException e) {
             logger.error("Failed to put message", e);
             event.getChannel().sendMessage("Sorry, something went wrong.").queue();
         }
@@ -91,27 +78,23 @@ public final class DatabaseListener extends ListenerAdapter implements AutoClose
         }
         String key = data[1];
 
-        try (PreparedStatement getValue = connection.prepareStatement(GET_VALUE)) {
-            getValue.setString(1, key);
-            try (ResultSet results = getValue.executeQuery()) {
-                if (!results.next()) {
-                    event.getChannel()
-                         .sendMessage("Nothing found for the key '" + key + "'")
-                         .queue();
-                    return;
-                }
-
-                String value = results.getString("value");
-                event.getChannel().sendMessage("Saved message: " + value).queue();
+        try {
+            Optional<StorageRecord> foundValue = database.read(ctx -> {
+                return ctx.selectFrom(Storage.STORAGE)
+                          .where(Storage.STORAGE.KEY.eq(key))
+                          .stream()
+                          .findFirst();
+            });
+            if (foundValue.isEmpty()) {
+                event.getChannel().sendMessage("Nothing found for the key '" + key + "'").queue();
+                return;
             }
-        } catch (SQLException e) {
+
+            String value = foundValue.get().getValue();
+            event.getChannel().sendMessage("Saved message: " + value).queue();
+        } catch (DatabaseException e) {
             logger.error("Failed to get message", e);
             event.getChannel().sendMessage("Sorry, something went wrong.").queue();
         }
-    }
-
-    @Override
-    public void close() throws Exception {
-        connection.close();
     }
 }
