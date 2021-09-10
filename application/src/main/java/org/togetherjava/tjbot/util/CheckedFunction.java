006acbf9cfddda8d2b8a84ede320c40c79d0805e
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-09-10 14:15:32 +0200

    Add jOOQ and Flyway
    This patch adds jOOQ, Flyway and a simple Database class. The Database
    class takes care of distributing the dsl context and write locking so
    sqlite should work without errors even in concurrent scenarios.
    
    Flyway transactions are registered under
    "application/src/main/resources/db/VX__Name_Of_The_Thing.sql".

---
+++ application/src/main/java/org/togetherjava/tjbot/util/CheckedFunction.java
@@ -0,0 +1,24 @@
+package org.togetherjava.tjbot.util;
+
+/**
+ * Represents a function that accepts a single input argument, returns a result and could possibly
+ * throw an exception.
+ *
+ * @param <T> the type of the input to the function
+ * @param <R> the type of the function's return value
+ * @param <E> the type of the exception
+ */
+@FunctionalInterface
+public interface CheckedFunction<T, R, E extends Throwable> {
+
+    /**
+     * Performs this operation on the given argument.
+     *
+     * @param t the input argument
+     * @return R on successful evaluation
+     * @throws E an exception if any occurs during the execution of the operation
+     */
+    R accept(T t) throws E;
+
+}
+
