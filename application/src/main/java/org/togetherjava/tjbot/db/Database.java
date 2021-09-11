7d6cb427482d655a7a26b32b4f9a0ffd394e9710
Zabuzard <zabuza.dev@gmail.com>
2021-09-11 11:45:21 +0200

    Various improvements to the DB code
    Mostly shown by linter, some are:
    * gitignore generated DB files (schema, ...)
    * try-with-resources for SELECT query
    * more generalized generics (? super ...)
    * locks have to be opened before the try-block
    * converted new Object() {...} into named helper class

--- application/src/main/java/org/togetherjava/tjbot/db/Database.java
+++ application/src/main/java/org/togetherjava/tjbot/db/Database.java
@@ -7,7 +7,6 @@ import org.jooq.SQLDialect;
 import org.jooq.exception.DataAccessException;
 import org.jooq.impl.DSL;
 import org.sqlite.SQLiteConfig;
-import org.sqlite.SQLiteConfig.JournalMode;
 import org.sqlite.SQLiteDataSource;
 import org.togetherjava.tjbot.util.CheckedConsumer;
 import org.togetherjava.tjbot.util.CheckedFunction;
@@ -19,7 +18,7 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * The main database class.
  */
-public class Database {
+public final class Database {
 
     private final DSLContext dslContext;
     private final Lock writeLock = new ReentrantLock();
@@ -34,7 +33,7 @@ public class Database {
         SQLiteConfig sqliteConfig = new SQLiteConfig();
         sqliteConfig.enforceForeignKeys(true);
         // In WAL mode only concurrent writes pose a problem so we synchronize on those
-        sqliteConfig.setJournalMode(JournalMode.WAL);
+        sqliteConfig.setJournalMode(SQLiteConfig.JournalMode.WAL);
 
         SQLiteDataSource dataSource = new SQLiteDataSource(sqliteConfig);
         dataSource.setUrl(jdbcUrl);
@@ -42,7 +41,7 @@ public class Database {
         Flyway flyway = Flyway.configure().dataSource(dataSource).locations("/db").load();
         flyway.migrate();
 
-        this.dslContext = DSL.using(dataSource.getConnection(), SQLDialect.SQLITE);
+        dslContext = DSL.using(dataSource.getConnection(), SQLDialect.SQLITE);
     }
 
     /**
@@ -51,7 +50,8 @@ public class Database {
      * @return a way to interact with the database in a read only way
      * @throws DatabaseException if an error occurs in the passed handler function
      */
-    public <T> T read(CheckedFunction<DSLContext, T, DataAccessException> action) {
+    public <T> T read(
+            CheckedFunction<? super DSLContext, T, ? extends DataAccessException> action) {
         try {
             return action.accept(getDslContext());
         } catch (DataAccessException e) {
@@ -64,9 +64,9 @@ public class Database {
      *
      * @throws DatabaseException if an error occurs in the passed handler function
      */
-    public void read(CheckedConsumer<DSLContext, DataAccessException> action) {
-        read(ctx -> {
-            action.accept(ctx);
+    public void read(CheckedConsumer<? super DSLContext, ? extends DataAccessException> action) {
+        read(context -> {
+            action.accept(context);
             return null;
         });
     }
@@ -77,9 +77,10 @@ public class Database {
      * @return a way to interact with the database
      * @throws DatabaseException if an error occurs in the passed handler function
      */
-    public <T> T write(CheckedFunction<DSLContext, T, DataAccessException> action) {
+    public <T> T write(
+            CheckedFunction<? super DSLContext, T, ? extends DataAccessException> action) {
+        writeLock.lock();
         try {
-            writeLock.lock();
             return action.accept(getDslContext());
         } catch (DataAccessException e) {
             throw new DatabaseException(e);
@@ -93,9 +94,9 @@ public class Database {
      *
      * @throws DatabaseException if an error occurs in the passed handler function
      */
-    public void write(CheckedConsumer<DSLContext, DataAccessException> action) {
-        write(ctx -> {
-            action.accept(ctx);
+    public void write(CheckedConsumer<? super DSLContext, ? extends DataAccessException> action) {
+        write(context -> {
+            action.accept(context);
             return null;
         });
     }
@@ -109,13 +110,12 @@ public class Database {
      * @return whatever the handler returned#
      * @throws DatabaseException if an error occurs in the passed handler function
      */
-    public <T> T readTransaction(CheckedFunction<DSLContext, T, DataAccessException> handler) {
-        var holder = new Object() {
-            T result;
-        };
+    public <T> T readTransaction(
+            CheckedFunction<? super DSLContext, T, DataAccessException> handler) {
+        var holder = new ResultHolder<T>();
 
         try {
-            getDslContext().transaction(cfg -> holder.result = handler.accept(cfg.dsl()));
+            getDslContext().transaction(config -> holder.result = handler.accept(config.dsl()));
         } catch (DataAccessException e) {
             throw new DatabaseException(e);
         }
@@ -130,9 +130,10 @@ public class Database {
      *        return value.
      * @throws DatabaseException if an error occurs in the passed handler function
      */
-    public void readTransaction(CheckedConsumer<DSLContext, DataAccessException> handler) {
-        readTransaction(db -> {
-            handler.accept(db);
+    public void readTransaction(
+            CheckedConsumer<? super DSLContext, ? extends DataAccessException> handler) {
+        readTransaction(dsl -> {
+            handler.accept(dsl);
             return null;
         });
     }
@@ -146,14 +147,13 @@ public class Database {
      * @return whatever the handler returned
      * @throws DatabaseException if an error occurs in the passed handler function
      */
-    public <T> T writeTransaction(CheckedFunction<DSLContext, T, DataAccessException> handler) {
-        var holder = new Object() {
-            T result;
-        };
+    public <T> T writeTransaction(
+            CheckedFunction<? super DSLContext, T, DataAccessException> handler) {
+        var holder = new ResultHolder<T>();
 
+        writeLock.lock();
         try {
-            writeLock.lock();
-            getDslContext().transaction(cfg -> holder.result = handler.accept(cfg.dsl()));
+            getDslContext().transaction(config -> holder.result = handler.accept(config.dsl()));
         } catch (DataAccessException e) {
             throw new DatabaseException(e);
         } finally {
@@ -170,9 +170,10 @@ public class Database {
      *        return value.
      * @throws DatabaseException if an error occurs in the passed handler function
      */
-    public void writeTransaction(CheckedConsumer<DSLContext, DataAccessException> handler) {
-        writeTransaction(db -> {
-            handler.accept(db);
+    public void writeTransaction(
+            CheckedConsumer<? super DSLContext, ? extends DataAccessException> handler) {
+        writeTransaction(dsl -> {
+            handler.accept(dsl);
             return null;
         });
     }
@@ -182,6 +183,16 @@ public class Database {
      */
     private DSLContext getDslContext() {
         return dslContext;
+    }
+
+    /**
+     * Utility classed used to wrap a result, for example to bypass <i>effectively final</i>
+     * restrictions.
+     *
+     * @param <T> the type of the result to hold
+     */
+    private static class ResultHolder<T> {
+        private T result;
     }
 }
 
--- application/src/main/java/org/togetherjava/tjbot/db/DatabaseException.java
+++ application/src/main/java/org/togetherjava/tjbot/db/DatabaseException.java
@@ -1,10 +1,16 @@
 package org.togetherjava.tjbot.db;
 
+import java.io.Serial;
+
 /**
  * Thrown when an error occurs while interacting with the database.
  */
 public class DatabaseException extends RuntimeException {
-    public DatabaseException(Throwable cause) {
+    /**
+     * Serial version UID.
+     */
+    @Serial
+    private static final long serialVersionUID = -4215197259643585552L;
         super(cause);
     }
 }
