51e36c3080d54e6bac2df959652ac5c51f5f912a
illuminator3 <hardt-j@web.de>
2021-09-13 18:59:31 +0200

    Fix test

--- formatter/src/test/java/org/togetherjava/formatter/tokenizer/LexerTest.java
+++ formatter/src/test/java/org/togetherjava/formatter/tokenizer/LexerTest.java
@@ -44,7 +44,7 @@ class LexerTest {
     @DisplayName("All tokens get tokenized correctly")
     void testInput() {
         String testString =
-                "class enum record interface import if for while null extends implements new return package this yield catch try else if else public private protected static sealed non-sealed final abstract void int long short byte boolean float double char ( ) { } [ ] ; :: : , += -= *= /= %= &= |= == != >= > <= < ! = ++ + -- - && || /* a */ / * . @annotation 0 \"string\" identifier ` ";
+                "class enum record interface import if for while null extends implements new return package this yield catch try else if else super public private protected static sealed non-sealed final abstract void int long short byte boolean float double char ( ) { } [ ] ; :: : , -> += -= *= /= %= &= |= == != >= > <= < ! = ++ + -- - && || /* a */ / * . ? @annotation 0 \"string\" identifier ` ";
         List<TokenType> tokens = lexer.tokenize(testString).stream().map(Token::type).toList(),
                 expected = new ArrayList<>();
 
