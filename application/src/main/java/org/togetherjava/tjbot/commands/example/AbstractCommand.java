5f90fdba172e7ab1f8e34f6c971b7d895189b49b
java-coding-prodigy <85149944+java-coding-prodigy@users.noreply.github.com>
2021-09-16 08:18:30 +0530

    Some minor changes for better readability

--- application/src/main/java/org/togetherjava/tjbot/commands/example/AbstractCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/example/AbstractCommand.java
@@ -4,37 +4,36 @@ import org.jetbrains.annotations.NotNull;
 import org.togetherjava.tjbot.commands.Command;
 
 /**
- * Makes usage of constructor to store the {@link #commandName}, {@link #description} and the
- * {@link #isGuildOnly} <br>
+ * Makes usage of constructor to store the {@link #commandName}, {@link #description} and the {@link #isGuildOnly} <br>
  * This helps against a tiny bit of duplicated code.
  */
 public class AbstractCommand implements Command {
-    private final String commandName;
-    private final String description;
-    private final boolean isGuildOnly;
+	private final String commandName;
+	private final String description;
+	private final boolean isGuildOnly;
 
-    public AbstractCommand(String commandName, String description, boolean isGuildOnly) {
-        this.commandName = commandName;
-        this.description = description;
-        this.isGuildOnly = isGuildOnly;
-    }
+	public AbstractCommand(String commandName, String description, boolean isGuildOnly) {
+		this.commandName = commandName;
+		this.description = description;
+		this.isGuildOnly = isGuildOnly;
+	}
 
-    public AbstractCommand(String commandName, String description) {
-        this(commandName, description, false);
-    }
+	public AbstractCommand(String commandName, String description) {
+		this(commandName, description, false);
+	}
 
-    @Override
-    public @NotNull String getCommandName() {
-        return commandName;
-    }
+	@Override
+	public @NotNull String getCommandName() {
+		return commandName;
+	}
 
-    @Override
-    public @NotNull String getDescription() {
-        return description;
-    }
+	@Override
+	public @NotNull String getDescription() {
+		return description;
+	}
 
-    @Override
-    public boolean isGuildOnly() {
-        return isGuildOnly;
-    }
+	@Override
+	public boolean isGuildOnly() {
+		return isGuildOnly;
+	}
 }
