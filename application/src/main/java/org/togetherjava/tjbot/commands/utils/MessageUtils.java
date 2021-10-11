f457845eef136c2c4cd9e378875b6a25b6fe91d7
talentedasian <lucenaalmirjames123@gmail.com>
2021-10-11 09:27:09 +0800

    Add private constructor that throws UnsupportedOperationException
    Add private constructor that throws UnsupportedOperationException

--- application/src/main/java/org/togetherjava/tjbot/commands/utils/MessageUtils.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/utils/MessageUtils.java
@@ -9,14 +9,18 @@ import java.util.List;
 /**
  * Utility methods for {@link Message}.
  * <p>
- * This class is meant to contain all utility methods for {@link Message} that can be used on all other commands
- * to avoid similar methods appearing everywhere.
+ * This class is meant to contain all utility methods for {@link Message} that can be used on all
+ * other commands to avoid similar methods appearing everywhere.
  */
 public class MessageUtils {
 
+    private MessageUtils() {
+        throw new UnsupportedOperationException();
+    }
+
     /**
-     * Disables all the buttons that a message has. Disabling buttons deems it as not clickable
-     * to the user who sees it.
+     * Disables all the buttons that a message has. Disabling buttons deems it as not clickable to
+     * the user who sees it.
      *
      * @param message the message that contains at least one button
      * @throws IllegalArgumentException when the given message does not contain any button
@@ -27,9 +31,8 @@ public class MessageUtils {
             throw new IllegalArgumentException("Message must contain at least one button");
 
         message
-                .editMessageComponents(
-                        ActionRow.of(buttons.stream().map(Button::asDisabled).toList()))
-                .queue();
+            .editMessageComponents(ActionRow.of(buttons.stream().map(Button::asDisabled).toList()))
+            .queue();
     }
 
 }
