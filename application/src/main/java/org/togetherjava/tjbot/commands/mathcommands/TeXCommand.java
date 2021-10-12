feb21b94863c5650b7822b5dabd715f9248966ae
java-coding-prodigy <java-coding-prodigy@github.com>
2021-10-12 10:05:35 +0530

    Using BAOS instead of Files

--- application/src/main/java/org/togetherjava/tjbot/commands/mathcommands/TeXCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/mathcommands/TeXCommand.java
@@ -2,7 +2,6 @@ package org.togetherjava.tjbot.commands.mathcommands;
 
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import org.jetbrains.annotations.NotNull;
 import org.scilab.forge.jlatexmath.ParseException;
 import org.scilab.forge.jlatexmath.TeXConstants;
@@ -15,8 +14,10 @@ import org.togetherjava.tjbot.commands.SlashCommandVisibility;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class TeXCommand extends SlashCommandAdapter {
 
@@ -27,23 +28,20 @@ public class TeXCommand extends SlashCommandAdapter {
 	public static final Color BACKGROUND_COLOR = new Color(BACKGROUND_HEX_CODE);
 	public static final int FOREGROUND_HEX_CODE = 0x01EC09;
 	public static final Color FOREGROUND_COLOR = new Color(FOREGROUND_HEX_CODE);
-	public static final File SENT_FILE =
-			new File("C:\\Users\\Abc\\IdeaProjects\\TJ-Bot\\application\\src\\main\\resources\\unknown.png");
 	public static final Logger logger = LoggerFactory.getLogger(TeXCommand.class);
 
-	public TeXCommand(){
-		super("/tex",
-				"This command accepts a latex expression and displays an image containing the mathematical expression of the input",
-				SlashCommandVisibility.GLOBAL);
-		getData().addOption(OptionType.STRING,"latex","The latex which is rendered as an image");
+	public TeXCommand() {
+		super("tex", "This command accepts a latex expression and generates an image corresponding to it.",
+				SlashCommandVisibility.GUILD);
+		getData().addOption(OptionType.STRING, "latex", "The latex which is rendered as an image");
 	}
-
 
 	@Override
 	public void onSlashCommand(@NotNull final SlashCommandEvent event) {
 		TeXFormula formula;
 		try {
-			formula = new TeXFormula(event.getOption("latex").getAsString());
+			formula = new TeXFormula(event.getOption("latex")
+					.getAsString());
 		} catch (ParseException pe) {
 			event.reply(INVALID_LATEX)
 					.setEphemeral(true)
@@ -53,14 +51,19 @@ public class TeXCommand extends SlashCommandAdapter {
 		BufferedImage image =
 				(BufferedImage) formula.createBufferedImage(TeXConstants.STYLE_DISPLAY, DEFAULT_IMAGE_SIZE,
 						FOREGROUND_COLOR, BACKGROUND_COLOR);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
 		try {
-			ImageIO.write(image, "png", SENT_FILE);
+			ImageIO.write(image, "png", baos);
 		} catch (IOException ioe) {
 			event.reply(RENDERING_ERROR)
 					.setEphemeral(true)
 					.queue();
 			return;
 		}
-		event.getChannel().sendFile(SENT_FILE).queue();
+		InputStream is = new ByteArrayInputStream(baos.toByteArray());
+		event.getChannel()
+				.sendFile(is, "unknown.png")
+				.queue();
 	}
 }
