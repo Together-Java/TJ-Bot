6c150a9786fb2c47f11648a9fc683bc17a2ceb32
java-coding-prodigy <java-coding-prodigy@github.com>
2021-10-13 13:17:19 +0530

    Formatted `Command.java`

--- application/src/main/java/org/togetherjava/tjbot/commands/Commands.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/Commands.java
@@ -10,30 +10,29 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * Utility class that offers all commands that should be registered by the system. New commands have
- * to be added here, where {@link org.togetherjava.tjbot.commands.system.CommandSystem} will then
- * pick it up from and register it with the system.
+ * Utility class that offers all commands that should be registered by the system. New commands have to be added here,
+ * where {@link org.togetherjava.tjbot.commands.system.CommandSystem} will then pick it up from and register it with the
+ * system.
  * <p>
- * To add a new slash command, extend the commands returned by
- * {@link #createSlashCommands(Database)}.
+ * To add a new slash command, extend the commands returned by {@link #createSlashCommands(Database)}.
  */
 public enum Commands {
-    ;
+	;
 
-    /**
-     * Creates all slash commands that should be registered with this application.
-     * <p>
-     * Calling this method multiple times will result in multiple commands being created, which
-     * generally should be avoided.
-     *
-     * @param database the database of the application, which commands can use to persist data
-     * @return a collection of all slash commands
-     */
-    public static @NotNull Collection<SlashCommand> createSlashCommands(
-            @NotNull Database database) {
-        // NOTE The command system can add special system relevant commands also by itself,
-        // hence this list may not necessarily represent the full list of all commands actually
-        // available.
-        return List.of(new PingCommand(), new DatabaseCommand(database), new TeXCommand());
-    }
+	/**
+	 * Creates all slash commands that should be registered with this application.
+	 * <p>
+	 * Calling this method multiple times will result in multiple commands being created, which generally should be
+	 * avoided.
+	 *
+	 * @param database the database of the application, which commands can use to persist data
+	 *
+	 * @return a collection of all slash commands
+	 */
+	public static @NotNull Collection<SlashCommand> createSlashCommands(@NotNull Database database) {
+		// NOTE The command system can add special system relevant commands also by itself,
+		// hence this list may not necessarily represent the full list of all commands actually
+		// available.
+		return List.of(new PingCommand(), new DatabaseCommand(database), new TeXCommand());
+	}
 }
