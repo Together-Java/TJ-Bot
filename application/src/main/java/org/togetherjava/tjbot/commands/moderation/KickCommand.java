246733552c8c6c4d9cf6549eaf846a96e577ce01
Yusuf <yusufgamer222@gmail.com>
2021-10-16 17:15:15 +0100

    Update KickCommand.java

--- application/src/main/java/org/togetherjava/tjbot/commands/moderation/KickCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/KickCommand.java
@@ -9,27 +9,44 @@ import org.jetbrains.annotations.NotNull;
 import org.togetherjava.tjbot.commands.SlashCommandAdapter;
 import org.togetherjava.tjbot.commands.SlashCommandVisibility;
 
-/**
- * @author RealYusufIsmail
- */
+
 public class KickCommand extends SlashCommandAdapter {
+    /**
+     * Creates an instance of the kick command.
+     */
     public KickCommand() {
         super("Kick", "Use this command to kick a user", SlashCommandVisibility.GUILD);
 
-        getData().addOption(OptionType.USER, "user", "The user which you want to kick", true);
+        getData().addOption(OptionType.USER, "user", "The user which you want to kick", true)
+                .addOption(OptionType.STRING, "reason", "The reason of the kick", true);
     }
 
+    /**
+     * When triggered with {@code /kick @user reason}, the bot will respond will check if the user
+     * has perms. Then it will check if itself has perms to kick. If it does it will check if the user is the user
+     * is too powerful or not. If the user is not then bot will kick the user.
+     *
+     * @param event the corresponding event
+     */
     @Override
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
         // Used to get the member
-        final Member member = event.getOption("user").getAsMember();
+        Member member = event.getOption("user").getAsMember();
 
-        event.deferReply(true).queue(); // Let the user know we received the command before doing
-                                        // anything else
-        InteractionHook hook = event.getHook(); // This is a special webhook that allows you to send
-                                                // messages without having permissions in the
-                                                // channel and also allows ephemeral messages
-        hook.setEphemeral(true); // All messages here will now be ephemeral implicitly
+        //Used for the reason
+        String reason = event.getOption("reason").getAsString();
+
+        // Let the user know we received the command before doing anything else
+        event.deferReply(true).queue();
+
+        // This is a special webhook that allows you to send
+        // messages without having permissions in the
+        // channel and also allows ephemeral messages
+        InteractionHook hook = event.getHook();
+
+        // All messages here will now be ephemeral implicitly
+        hook.setEphemeral(true);
+
 
         // Checks if the author has perms
         if (!event.getMember().hasPermission(Permission.KICK_MEMBERS)) {
@@ -56,8 +73,8 @@ public class KickCommand extends SlashCommandAdapter {
 
         // Kicks the user and send a success response
         event.getGuild()
-            .kick(member)
-            .flatMap(v -> hook.sendMessage("Kicked the user" + member.getUser()))
+            .kick(member, reason)
+            .flatMap(v -> event.reply("Kicked the user" + member.getUser().getAsTag()))
             .queue();
     }
 }
