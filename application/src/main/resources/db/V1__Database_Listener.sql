d054f3acd9da0fd9fcf311158e476dbf0d7116c1
Yusuf <yusufgamer222@gmail.com>
2021-10-17 15:45:33 +0100

    Update V1__Database_Listener.sql

--- application/src/main/resources/db/V1__Database_Listener.sql
+++ application/src/main/resources/db/V1__Database_Listener.sql
@@ -3,3 +3,9 @@ CREATE TABLE storage
     key   TEXT NOT NULL PRIMARY KEY,
     value TEXT NOT NULL
 )
+CREATE TABLE warn
+(
+    user_id  INTEGER  NOT NULL,
+    guild_id INTEGER  NOT NULL,
+    warns    INTEGER  NOT NULL
+)
