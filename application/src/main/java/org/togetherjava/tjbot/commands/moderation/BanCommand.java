48f98634af703c890ec447ee0e2d7a935caf6c5b
Yusuf <yusufgamer222@gmail.com>
2021-10-17 12:37:13 +0100

    Update BanCommand.java

--- application/src/main/java/org/togetherjava/tjbot/commands/moderation/BanCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/BanCommand.java
@@ -36,7 +36,8 @@ public final class BanCommand extends SlashCommandAdapter {
 
         getData().addOption(OptionType.USER, USER_OPTION, "The user which you want to ban", true)
             .addOption(OptionType.INTEGER, DELETE_MESSAGE_HISTORY_DAYS_OPTION,
-                    "The delete message history. Between 1 to as many days as you want", true)
+                    "The messages in these day will be deleted. 1 to as many days as you want",
+                    true)
             .addOption(OptionType.STRING, REASON_OPTION, "The reason of the ban", true);
     }
 
