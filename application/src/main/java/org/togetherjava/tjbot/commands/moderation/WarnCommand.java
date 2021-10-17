c3f3c9a19664de6609430813b7c9c8f6ad681e37
Yusuf <yusufgamer222@gmail.com>
2021-10-17 11:53:21 +0100

    Create WarnCommand.java

---
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/WarnCommand.java
@@ -0,0 +1,24 @@
+package org.togetherjava.tjbot.commands.moderation;
+
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import org.jetbrains.annotations.NotNull;
+import org.togetherjava.tjbot.commands.SlashCommandAdapter;
+import org.togetherjava.tjbot.commands.SlashCommandVisibility;
+
+public class WarnCommand extends SlashCommandAdapter {
+    private static final String REASON_OPTION = "reason";
+    private static final String USER_OPTION = "user";
+
+    protected WarnCommand() {
+        super("warn", "Use this command to warn", SlashCommandVisibility.GUILD);
+
+        getData().addOption(OptionType.USER, USER_OPTION, "The user which you want to warn", true)
+                .addOption(OptionType.STRING, REASON_OPTION, "The reason of the warb", true);
+    }
+
+    @Override
+    public void onSlashCommand(@NotNull SlashCommandEvent event) {
+
+    }
+}
