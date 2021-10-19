014207b46b109cc6022e28a7ab1241a9b213dfbd
krankkk <59603675+krankkkk@users.noreply.github.com>
2021-10-19 19:43:31 +0200

    Add JavaDoc for new Util Methods

--- logviewer/src/main/java/org/togetherjava/tjbot/logwatcher/util/LogUtils.java
+++ logviewer/src/main/java/org/togetherjava/tjbot/logwatcher/util/LogUtils.java
@@ -10,6 +10,9 @@ import java.util.stream.Collectors;
 
 public final class LogUtils {
 
+    /**
+     * Loglevel of all Configurable Levels in the logger
+     */
     public enum LogLevel {
         INFO,
         WARN,
@@ -17,15 +20,32 @@ public final class LogUtils {
         DEBUG,
         TRACE;
 
+        /**
+         * Collects all LogLevel in a Set
+         * 
+         * @return A Set containing every Loglevel
+         */
         public static Set<LogLevel> getAll() {
             return EnumSet.allOf(LogLevel.class);
         }
 
+        /**
+         * Maps the LogLevel to their name and collects it in a Set
+         *
+         * @return A Set containing every LogLevel as String
+         */
         public static Set<String> getAllNames() {
             return Arrays.stream(values()).map(Enum::name).collect(Collectors.toUnmodifiableSet());
         }
     }
 
+    /**
+     * Maps a Logevent to the color-coded css-Class (css class has i.e. red as background-color for
+     * ERROR events)
+     *
+     * @param event Logevent from the DB with a specific Logevent
+     * @return The name of the CSS class to use with this Logevent.
+     */
     public static String logLevelToCssClass(final Logevents event) {
         return event.getLevel().toLowerCase(Locale.ENGLISH);
     }
