198c720b08b19a73660532dc5837d01f21fc52ac
Yusuf <yusufgamer222@gmail.com>
2021-10-21 21:13:18 +0100

    Update BanHelperMethods.java

--- application/src/main/java/org/togetherjava/tjbot/commands/moderation/BanHelperMethods.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/BanHelperMethods.java
@@ -40,7 +40,7 @@ public class BanHelperMethods {
             .openPrivateChannelById(userId)
             .flatMap(channel -> channel.sendMessage(
                     """
-                    Hey there, sorry to tell you but unfortunately you have been banned from the guild 'Together Java'. 
+                    Hey there, sorry to tell you but unfortunately you have been banned from the guild 'Together Java'.\040
                     If you think this was a mistake, please contact a moderator or admin of the guild.
                     The ban reason is:  %s
                     """.formatted(reason)))
