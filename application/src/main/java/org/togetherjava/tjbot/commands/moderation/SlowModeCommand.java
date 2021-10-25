19ca97eb8f32bf6ee5e4bae7fd2e30185a9f41fa
Yusuf <yusufgamer222@gmail.com>
2021-10-25 20:23:17 +0100

    Update SlowModeCommand.java

--- application/src/main/java/org/togetherjava/tjbot/commands/moderation/SlowModeCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/SlowModeCommand.java
@@ -2,11 +2,12 @@ package org.togetherjava.tjbot.commands.moderation;
 
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.managers.ChannelManager;
-import net.dv8tion.jda.api.managers.Manager;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.togetherjava.tjbot.commands.SlashCommandAdapter;
 import org.togetherjava.tjbot.commands.SlashCommandVisibility;
 
@@ -14,6 +15,7 @@ import java.util.Objects;
 
 public class SlowModeCommand extends SlashCommandAdapter {
     private static final String NUMBER_OF_SECONDS = "number_of_seconds";
+    private static final Logger logger = LoggerFactory.getLogger(SlowModeCommand.class);
 
     public SlowModeCommand() {
         super("slow_mode", "Use this command to set a slow mode", SlashCommandVisibility.GUILD);
@@ -24,6 +26,7 @@ public class SlowModeCommand extends SlashCommandAdapter {
     @Override
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
         final Member author = event.getMember();
+        final TextChannel channel = event.getTextChannel();
 
         if (!Objects.requireNonNull(author).hasPermission(Permission.MANAGE_CHANNEL)) {
             event.reply("You are missing MANAGE_CHANNEL permission to delete these the message")
@@ -38,12 +41,15 @@ public class SlowModeCommand extends SlashCommandAdapter {
             event.reply("I am missing MANAGE_CHANNEL permission to delete these messages")
                     .setEphemeral(true)
                     .queue();
+
+            logger.error("The bot does not have BAN_MEMBERS permission on the server '{}' ",
+                    Objects.requireNonNull(event.getGuild()));
             return;
         }
 
         int slowModeTime = Math.toIntExact(
                 Objects.requireNonNull(event.getOption(NUMBER_OF_SECONDS)).getAsLong());
 
-        Objects.requireNonNull(event.getGuild().getDefaultChannel()).getManager().setSlowmode(slowModeTime);
+        channel.getManager().setSlowmode(slowModeTime).queue();
     }
 }
