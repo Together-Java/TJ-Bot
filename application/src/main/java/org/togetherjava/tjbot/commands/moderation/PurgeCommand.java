c4f5e54c22dcb25663d04c94f05de737e08d2478
Yusuf <yusufgamer222@gmail.com>
2021-10-30 13:19:00 +0100

    Update PurgeCommand.java

--- application/src/main/java/org/togetherjava/tjbot/commands/moderation/PurgeCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/PurgeCommand.java
@@ -1,7 +1,9 @@
 package org.togetherjava.tjbot.commands.moderation;
 
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.ISnowflake;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -11,7 +13,12 @@ import org.slf4j.LoggerFactory;
 import org.togetherjava.tjbot.commands.SlashCommandAdapter;
 import org.togetherjava.tjbot.commands.SlashCommandVisibility;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * This command can remove specific messages using their ids. The command requires the user to input
@@ -21,6 +28,7 @@ import java.util.Objects;
  * permission to delete messages.
  */
 public class PurgeCommand extends SlashCommandAdapter {
+    // TODO Test the command
     private static final Logger logger = LoggerFactory.getLogger(PurgeCommand.class);
     private static final String FIRST_MESSAGE_ID = "first_message_id";
     private static final String LAST_MESSAGE_ID = "last_message_id";
@@ -30,30 +38,29 @@ public class PurgeCommand extends SlashCommandAdapter {
         super("purge", "Delete specific messages using there ids.", SlashCommandVisibility.GUILD);
 
         getData()
-            .addOption(OptionType.NUMBER, FIRST_MESSAGE_ID,
-                    "The id of the message you want to delete from.", true)
-            .addOption(OptionType.NUMBER, LAST_MESSAGE_ID,
-                    "The id of the message you want to delete to.", true);
+                .addOption(OptionType.NUMBER, FIRST_MESSAGE_ID,
+                        "The id of the message you want to delete from.", true)
+                .addOption(OptionType.NUMBER, LAST_MESSAGE_ID,
+                        "The id of the message you want to delete to.", true);
     }
 
     @Override
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
         final Member author = event.getMember();
-        final TextChannel channel = event.getTextChannel();
 
-        if (!Objects.requireNonNull(author).hasPermission(Permission.MESSAGE_MANAGE)) {
+        if (!author.hasPermission(Permission.MESSAGE_MANAGE)) {
             event.reply("You are missing MESSAGE_MANAGE permission to delete these the message")
-                .setEphemeral(true)
-                .queue();
+                    .setEphemeral(true)
+                    .queue();
             return;
         }
 
         final Member bot = Objects.requireNonNull(event.getGuild()).getSelfMember();
         if (!bot.hasPermission(Permission.MESSAGE_MANAGE)) {
             event.reply(
-                    "I am missing MESSAGE_MANAGE permission which means I am unable to delete messages in this server.")
-                .setEphemeral(true)
-                .queue();
+                            "I am missing MESSAGE_MANAGE permission which means I am unable to delete messages in this server.")
+                    .setEphemeral(true)
+                    .queue();
 
             logger.error("The bot does not have MESSAGE_MANAGE permission on the server '{}' ",
                     event.getGuild().getId());
@@ -63,19 +70,39 @@ public class PurgeCommand extends SlashCommandAdapter {
         long firstMessageId = Objects.requireNonNull(event.getOption(FIRST_MESSAGE_ID)).getAsLong();
         long lastMessageId = Objects.requireNonNull(event.getOption(LAST_MESSAGE_ID)).getAsLong();
 
-        deleteMessagesById(channel, author, firstMessageId, lastMessageId, event);
+        deleteMessagesById(firstMessageId, lastMessageId, event.getMessageChannel(), strings -> {
+
+        });
     }
 
-    public static void deleteMessagesById(@NotNull TextChannel channel, @NotNull Member author,
-            long firstMessageId, long lastMessageId, @NotNull SlashCommandEvent event) {
+    private static void deleteMessagesById(long firstMessageId, long lastMessageId,
+                                           MessageChannel mc, Consumer<List<String>> cb) {
+        getBetween0(firstMessageId, lastMessageId, mc, new ArrayList<>(), new ReentrantLock(), cb);
+    }
 
-        channel.purgeMessagesById(firstMessageId, lastMessageId);
+    private static void getBetween0(long firstMessageId, long lastMessageId, MessageChannel mc,
+                                    List<String> acc, ReentrantLock lock, Consumer<List<String>> cb) {
+        mc.getHistoryBefore(firstMessageId, 100).queue(his -> {
+            try {
+                lock.lock();
+                List<String> collected = his.getRetrievedHistory()
+                        .stream()
+                        .map(ISnowflake::getId)
+                        .collect(Collectors.toList());
 
-        event.reply("I have deleted the messages from " + firstMessageId + " to " + lastMessageId)
-            .setEphemeral(true)
-            .queue();
+                int foundIdx = collected.indexOf(String.valueOf(lastMessageId));
 
-        logger.info(" '{}' deleted messages from '{}' till '{}", author, firstMessageId,
-                lastMessageId);
+                if (foundIdx != -1) {
+                    acc.addAll(collected.subList(0, foundIdx));
+                    cb.accept(acc);
+                } else {
+                    acc.addAll(collected);
+                    getBetween0(Long.parseLong(collected.get(collected.size() - 1)), lastMessageId,
+                            mc, acc, lock, cb);
+                }
+            } finally {
+                lock.unlock();
+            }
+        });
     }
 }
