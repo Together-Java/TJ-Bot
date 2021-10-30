af6a6ad64a17318f84d9b528eebdae7d2228bea5
Yusuf <yusufgamer222@gmail.com>
2021-10-30 13:24:41 +0100

    spotlessApply

--- application/src/main/java/org/togetherjava/tjbot/commands/moderation/PurgeCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/PurgeCommand.java
@@ -38,10 +38,10 @@ public class PurgeCommand extends SlashCommandAdapter {
         super("purge", "Delete specific messages using there ids.", SlashCommandVisibility.GUILD);
 
         getData()
-                .addOption(OptionType.NUMBER, FIRST_MESSAGE_ID,
-                        "The id of the message you want to delete from.", true)
-                .addOption(OptionType.NUMBER, LAST_MESSAGE_ID,
-                        "The id of the message you want to delete to.", true);
+            .addOption(OptionType.NUMBER, FIRST_MESSAGE_ID,
+                    "The id of the message you want to delete from.", true)
+            .addOption(OptionType.NUMBER, LAST_MESSAGE_ID,
+                    "The id of the message you want to delete to.", true);
     }
 
     @Override
@@ -50,17 +50,17 @@ public class PurgeCommand extends SlashCommandAdapter {
 
         if (!author.hasPermission(Permission.MESSAGE_MANAGE)) {
             event.reply("You are missing MESSAGE_MANAGE permission to delete these the message")
-                    .setEphemeral(true)
-                    .queue();
+                .setEphemeral(true)
+                .queue();
             return;
         }
 
         final Member bot = Objects.requireNonNull(event.getGuild()).getSelfMember();
         if (!bot.hasPermission(Permission.MESSAGE_MANAGE)) {
             event.reply(
-                            "I am missing MESSAGE_MANAGE permission which means I am unable to delete messages in this server.")
-                    .setEphemeral(true)
-                    .queue();
+                    "I am missing MESSAGE_MANAGE permission which means I am unable to delete messages in this server.")
+                .setEphemeral(true)
+                .queue();
 
             logger.error("The bot does not have MESSAGE_MANAGE permission on the server '{}' ",
                     event.getGuild().getId());
@@ -76,19 +76,19 @@ public class PurgeCommand extends SlashCommandAdapter {
     }
 
     private static void deleteMessagesById(long firstMessageId, long lastMessageId,
-                                           MessageChannel mc, Consumer<List<String>> cb) {
+            MessageChannel mc, Consumer<List<String>> cb) {
         getBetween0(firstMessageId, lastMessageId, mc, new ArrayList<>(), new ReentrantLock(), cb);
     }
 
     private static void getBetween0(long firstMessageId, long lastMessageId, MessageChannel mc,
-                                    List<String> acc, ReentrantLock lock, Consumer<List<String>> cb) {
+            List<String> acc, ReentrantLock lock, Consumer<List<String>> cb) {
         mc.getHistoryBefore(firstMessageId, 100).queue(his -> {
             try {
                 lock.lock();
                 List<String> collected = his.getRetrievedHistory()
-                        .stream()
-                        .map(ISnowflake::getId)
-                        .collect(Collectors.toList());
+                    .stream()
+                    .map(ISnowflake::getId)
+                    .collect(Collectors.toList());
 
                 int foundIdx = collected.indexOf(String.valueOf(lastMessageId));
 
