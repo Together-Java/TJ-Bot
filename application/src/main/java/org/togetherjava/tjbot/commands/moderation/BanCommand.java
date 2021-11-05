a1c12f760d112af32712aa7a771f9df4968b8511
Zabuzard <zabuza.dev@gmail.com>
2021-11-05 13:49:54 +0100

    Not banning already banned users

--- application/src/main/java/org/togetherjava/tjbot/commands/moderation/BanCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/BanCommand.java
@@ -5,9 +5,14 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
+import net.dv8tion.jda.api.interactions.Interaction;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.requests.ErrorResponse;
+import net.dv8tion.jda.api.requests.RestAction;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +20,7 @@ import org.togetherjava.tjbot.commands.SlashCommandAdapter;
 import org.togetherjava.tjbot.commands.SlashCommandVisibility;
 
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * This command can ban users and optionally remove their messages from the past days. Banning can
@@ -43,11 +49,22 @@ public final class BanCommand extends SlashCommandAdapter {
                     true).addChoice("none", 0).addChoice("recent", 1).addChoice("all", 7));
     }
 
+    private static RestAction<InteractionHook> handleAlreadyBanned(@NotNull Guild.Ban ban,
+            @NotNull Interaction event) {
+        String reason = ban.getReason();
+        String reasonText =
+                reason == null || reason.isBlank() ? "" : " (reason: %s)".formatted(reason);
+
+        String message = "The user '%s' is already banned%s.".formatted(ban.getUser().getAsTag(),
+                reasonText);
+        return event.reply(message).setEphemeral(true);
+    }
+
     @SuppressWarnings("MethodWithTooManyParameters")
-    private static void banUser(@NotNull User target, @NotNull Member author,
+    private static RestAction<InteractionHook> banUser(@NotNull User target, @NotNull Member author,
             @NotNull String reason, int deleteHistoryDays, @NotNull Guild guild,
             @NotNull SlashCommandEvent event) {
-        event.getJDA()
+        return event.getJDA()
             .openPrivateChannelById(target.getIdLong())
             .flatMap(channel -> channel.sendMessage(
                     """
@@ -57,22 +74,47 @@ public final class BanCommand extends SlashCommandAdapter {
                             """
                         .formatted(guild.getName(), reason)))
             .mapToResult()
-            .flatMap(result -> guild.ban(target, deleteHistoryDays, reason))
-            .flatMap(result -> event.reply("'%s' was banned by '%s' for: %s"
-                .formatted(target.getAsTag(), author.getUser().getAsTag(), reason)))
-            .queue();
+            .flatMap(result -> {
+                logger.info(
+                        "'{}' ({}) banned the user '{}' ({}) from guild '{}' and deleted their message history of the last {} days, for reason '{}'",
+                        author.getUser().getAsTag(), author.getId(), target.getAsTag(),
+                        target.getId(), guild.getName(), deleteHistoryDays, reason);
 
-        logger.info(
-                "'{}' ({}) banned the user '{}' ({}) from guild '{}' and deleted their message history of the last {} days, for reason '{}'",
-                author.getUser().getAsTag(), author.getId(), target.getAsTag(), target.getId(),
-                guild.getName(), deleteHistoryDays, reason);
+                return guild.ban(target, deleteHistoryDays, reason);
+            })
+            .flatMap(result -> event.reply("'%s' was banned by '%s' for: %s"
+                .formatted(target.getAsTag(), author.getUser().getAsTag(), reason)));
+    }
+
+    private static Optional<RestAction<InteractionHook>> handleNotAlreadyBannedResponse(
+            @NotNull Throwable alreadyBannedFailure, @NotNull Interaction event,
+            @NotNull Guild guild, @NotNull User target) {
+        if (alreadyBannedFailure instanceof ErrorResponseException errorResponseException) {
+            if (errorResponseException.getErrorResponse() == ErrorResponse.UNKNOWN_BAN) {
+                return Optional.empty();
+            }
+
+            if (errorResponseException.getErrorResponse() == ErrorResponse.MISSING_PERMISSIONS) {
+                logger.error("The bot does not have the '{}' permission on the guild '{}'.",
+                        Permission.BAN_MEMBERS, guild.getName());
+                return Optional.of(event.reply(
+                        "I can not ban users in this guild since I do not have the %s permission."
+                            .formatted(Permission.BAN_MEMBERS))
+                    .setEphemeral(true));
+            }
+        }
+        logger.warn("Something unexpected went wrong while trying to ban the user '{}'",
+                target.getAsTag(), alreadyBannedFailure);
+        return Optional.of(event.reply("Failed to ban the user due to an unexpected problem.")
+            .setEphemeral(true));
     }
 
     @Override
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
         OptionMapping targetOption =
                 Objects.requireNonNull(event.getOption(TARGET_OPTION), "The target is null");
-        Member target = targetOption.getAsMember();
+        Member targetMember = targetOption.getAsMember();
+        User targetUser = targetOption.getAsUser();
         Member author = Objects.requireNonNull(event.getMember(), "The author is null");
         String reason = Objects.requireNonNull(event.getOption(REASON_OPTION), "The reason is null")
             .getAsString();
@@ -81,8 +123,8 @@ public final class BanCommand extends SlashCommandAdapter {
         Member bot = guild.getSelfMember();
 
         // Member doesn't exist if attempting to ban a user who is not part of the guild.
-        if (target != null && !ModerationUtils.handleCanInteractWithTarget("ban", bot, author,
-                target, event)) {
+        if (targetMember != null && !ModerationUtils.handleCanInteractWithTarget("ban", bot, author,
+                targetMember, event)) {
             return;
         }
         if (!ModerationUtils.handleHasPermissions("ban", Permission.BAN_MEMBERS, bot, author, guild,
@@ -96,6 +138,15 @@ public final class BanCommand extends SlashCommandAdapter {
         int deleteHistoryDays = Math
             .toIntExact(Objects.requireNonNull(event.getOption(DELETE_HISTORY_OPTION)).getAsLong());
 
-        banUser(targetOption.getAsUser(), author, reason, deleteHistoryDays, guild, event);
+        // Ban the user, but only if not already banned
+        guild.retrieveBan(targetUser).mapToResult().flatMap(alreadyBanned -> {
+            if (alreadyBanned.isSuccess()) {
+                return handleAlreadyBanned(alreadyBanned.get(), event);
+            }
+
+            return handleNotAlreadyBannedResponse(Objects
+                .requireNonNull(alreadyBanned.getFailure()), event, guild, targetUser).orElseGet(
+                        () -> banUser(targetUser, author, reason, deleteHistoryDays, guild, event));
+        }).queue();
     }
 }
