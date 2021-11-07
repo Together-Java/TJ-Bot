bb84411506c7b03f0d67905b3ec300e8ba2a99c6
borgrel <borgrel@aol.com>
2021-11-07 13:08:22 +0200

    Changed UserStrings to TextBlocks

--- application/src/main/java/org/togetherjava/tjbot/commands/free/UserStrings.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/free/UserStrings.java
@@ -16,14 +16,21 @@ public enum UserStrings {
 
             __Please do not post your question in other channels__
             """),
-    MARK_AS_FREE("This channel is now free for a question to be asked."),
-    ALREADY_FREE_ERROR("This channel is already free, no changes made"),
-    NOT_READY_ERROR("Command not ready please try again in a minute"),
-    NOT_MONITORED_ERROR("This channel is not being monitored for free/busy status."
-            + " If you believe this channel should be part of the free/busy status system,"
-            + " please discuss it with a moderator"),
-    NOT_CONFIGURED_ERROR("This guild (%s) is not configured to use the '/free' command,"
-            + " please add entries in the config, restart the bot and try again.");
+    MARK_AS_FREE("""
+            This channel is now free for a question to be asked.
+            """),
+    ALREADY_FREE_ERROR("""
+            This channel is already free, no changes made
+            """),
+    NOT_READY_ERROR("""
+            Command not ready please try again in a minute
+            """),
+    NOT_MONITORED_ERROR("""
+            This channel is not being monitored for free/busy status. If you believe this channel should be part of the free/busy status system, please discuss it with a moderator
+            """),
+    NOT_CONFIGURED_ERROR("""
+            This guild (%s) is not configured to use the '/free' command, please add entries in the config, restart the bot and try again.
+            """);
 
     private final String message;
 
