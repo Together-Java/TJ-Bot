f21116a95e9177737503a437a9a7dd7d32bb681e
illuminator3 <hardt-j@web.de>
2021-11-12 16:38:45 +0100

    Expand javadoc

--- inmemorycompiler/src/main/java/org/togetherjava/tjbot/imc/package-info.java
+++ inmemorycompiler/src/main/java/org/togetherjava/tjbot/imc/package-info.java
@@ -1,4 +1,4 @@
 /**
- * This package provides utility classes to compile Java code fully in memory.
+ * This package provides utility classes to compile Java code fully in memory. (IMC stands for In-Memory-Compiler)
  */
 package org.togetherjava.tjbot.imc;
