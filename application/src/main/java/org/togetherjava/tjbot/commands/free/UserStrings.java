1c80abf219449e1eaa831baced90f63a1c3e7c7d
borgrel <borgrel@aol.com>
2021-11-13 21:06:14 +0200

    fixed import

--- application/src/main/java/org/togetherjava/tjbot/commands/free/UserStrings.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/free/UserStrings.java
@@ -1,8 +1,7 @@
 package org.togetherjava.tjbot.commands.free;
 
 import org.jetbrains.annotations.NotNull;
-
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Class containing all the strings sent to users during their interaction with the free command
