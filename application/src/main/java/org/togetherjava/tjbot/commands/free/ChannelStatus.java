d50b78e89902c863556485dfc9425bd725f2f7f8
borgrel <borgrel@aol.com>
2021-11-13 21:34:51 +0200

    improved javadocs

--- application/src/main/java/org/togetherjava/tjbot/commands/free/ChannelStatus.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/free/ChannelStatus.java
@@ -100,6 +100,12 @@ public final class ChannelStatus {
      * safe.
      *
      * @param guild the {@link Guild} that the channel belongs to, to retrieve its name from.
+     * @throws IllegalArgumentException if the guild has not been added, see
+     *         {@link ChannelMonitor#addChannelForStatus(TextChannel)}
+     * @throws IllegalStateException if a channel was added, see
+     *         {@link ChannelMonitor#addChannelToMonitor(long)}, that is not a {@link TextChannel}.
+     *         Since addChannelToMonitor does not access the {@link JDA} the entry can only be
+     *         validated before use instead of on addition.
      */
     public void updateChannelName(@NotNull final Guild guild) {
         GuildChannel channel = guild.getGuildChannelById(channelId);
