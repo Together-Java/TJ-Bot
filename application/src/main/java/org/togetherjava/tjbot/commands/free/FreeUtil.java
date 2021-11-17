94ce8f1fd196276ebb4597c6aa603d36d1fa9f16
borgrel <borgrel@aol.com>
2021-11-17 19:46:14 +0200

    changed inactive to work when retrieveHistory fails

--- application/src/main/java/org/togetherjava/tjbot/commands/free/FreeUtil.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/free/FreeUtil.java
@@ -1,16 +1,24 @@
 package org.togetherjava.tjbot.commands.free;
 
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.interactions.Interaction;
+import net.dv8tion.jda.api.utils.TimeUtil;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.togetherjava.tjbot.config.FreeCommandConfig;
 
 import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * A class containing helper methods required by the free package command.
  */
 enum FreeUtil {
     ;
+    private static final Logger logger = LoggerFactory.getLogger(FreeUtil.class);
 
     /**
      * Helper method to easily send ephemeral messages to users.
@@ -22,6 +30,64 @@ enum FreeUtil {
         interaction.reply(message).setEphemeral(true).queue();
     }
 
+    /**
+     * Method that provides the message history of a {@link TextChannel}.
+     * <p>
+     * </p>
+     * This method attempts to retrieve the message history, and logs any problems that occur in the
+     * attempt.
+     *
+     * @param channel the channel from which the history is required.
+     * @param limit the number of messages to retrieve.
+     * @return the requested message history or empty if unable to.
+     */
+    public static @NotNull Optional<List<Message>> getChannelHistory(@NotNull TextChannel channel,
+            final int limit) {
+        return Optional
+            .ofNullable(channel.getHistory().retrievePast(limit).mapToResult().map(listResult -> {
+                if (listResult.isFailure()) {
+                    logger.error("Failed to retrieve messages from %s because of:"
+                        .formatted(channel.getAsMention()), listResult.getFailure());
+                    return null;
+                } else {
+                    return listResult.get();
+                }
+            }).complete());
+    }
+
+    /**
+     * Method that provides the id of the latest message in a {@link TextChannel}.
+     * <p>
+     * This method tests for problems with retrieving the id like the latest message was deleted and
+     * the channel history being empty (or network trouble), etc.
+     *
+     * @param channel the channel from which the latest message is required.
+     * @return the id of the latest message or empty if it could not be retrieved.
+     */
+    public static @NotNull Optional<Long> getLastMessageId(@NotNull TextChannel channel) {
+        return Optional
+            .ofNullable(channel.hasLatestMessage() ? channel.getLatestMessageIdLong() : null)
+            .or(() -> getChannelHistory(channel, 1)
+                .map(list -> !list.isEmpty() ? list.get(0).getIdLong() : null));
+    }
+
+    /**
+     * Method that returns the time data from a discord snowflake.
+     *
+     * @param id the snowflake containing the time desired
+     * @return the creation time of the entity the id represents
+     */
+    public static @NotNull OffsetDateTime timeFromId(long id) {
+        return TimeUtil.getTimeCreated(id);
+    }
+
+    /**
+     * Method that calculates a time value a specific duration before now. The duration is
+     * configured in {@link FreeCommandConfig#INACTIVE_UNIT} and
+     * {@link FreeCommandConfig#INACTIVE_DURATION}.
+     * 
+     * @return the time value a set duration before now.
+     */
     public static @NotNull OffsetDateTime inactiveTimeLimit() {
         return OffsetDateTime.now()
             .minus(FreeCommandConfig.INACTIVE_DURATION, FreeCommandConfig.INACTIVE_UNIT);
