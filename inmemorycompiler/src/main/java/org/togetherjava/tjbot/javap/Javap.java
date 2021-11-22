8879202554e09987c3c11796b4abc1e72e9e3734
illuminator3 <hardt-j@web.de>
2021-11-22 19:56:57 +0100

    Change array creation to object cast

--- inmemorycompiler/src/main/java/org/togetherjava/tjbot/javap/Javap.java
+++ inmemorycompiler/src/main/java/org/togetherjava/tjbot/javap/Javap.java
@@ -90,7 +90,7 @@ public final class Javap {
             invokeOptions.add(TEMP_FILE_NAME);
 
             javapTaskRunMethod.invoke(javapTaskInstance,
-                    new Object[] {invokeOptions.toArray(new String[0])});
+                    (Object) invokeOptions.toArray(new String[0]));
         } catch (InvocationTargetException | NoSuchMethodException | InstantiationException
                 | IllegalAccessException ex) {
             throw new ReflectionException("A fatal exception has occurred while using reflection",
