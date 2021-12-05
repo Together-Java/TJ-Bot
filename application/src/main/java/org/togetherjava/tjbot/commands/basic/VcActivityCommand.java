8369806c0d4aff3ef6d8fb42744ec7ac26474efb
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2021-12-05 21:22:04 +0000

    Changed to Time unit

--- application/src/main/java/org/togetherjava/tjbot/commands/basic/VcActivityCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/basic/VcActivityCommand.java
@@ -40,7 +40,7 @@ public final class VcActivityCommand extends SlashCommandAdapter {
     private static final Logger logger = LoggerFactory.getLogger(VcActivityCommand.class);
 
     private static final int MIN_AGE_DURATION = 0;
-    private static final Duration MAX_AGE_DURATION = ChronoUnit.DAYS.getDuration();
+    private static final long MAX_AGE_DURATION = TimeUnit.DAYS.toSeconds(7);
 
     private static final int MIN_USE_AMOUNT = 0;
     private static final int MAX_USE_AMOUNT = 100;
@@ -96,8 +96,7 @@ public final class VcActivityCommand extends SlashCommandAdapter {
                         .setRequiredRange(MIN_USE_AMOUNT, MAX_USE_AMOUNT),
             new OptionData(OptionType.INTEGER, MAX_AGE_OPTION,
                     "Max age in seconds. Set this to 0 to never expire, default is 1 day", false)
-                        .setRequiredRange(MIN_AGE_DURATION,
-                                MAX_AGE_DURATION.getSeconds()));
+                        .setRequiredRange(MIN_AGE_DURATION, MAX_AGE_DURATION);
 
 
     /**
