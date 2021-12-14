533afa115dc6cba1e2e5cb9d7f7a4117558b23b4
Yusuf <yusufgamer222@gmail.com>
2021-12-14 20:34:26 +0000

    improved the ping command

--- application/src/main/java/org/togetherjava/tjbot/commands/basic/PingCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/basic/PingCommand.java
@@ -1,30 +1,37 @@
 package org.togetherjava.tjbot.commands.basic;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import org.jetbrains.annotations.NotNull;
 import org.togetherjava.tjbot.commands.SlashCommandAdapter;
 import org.togetherjava.tjbot.commands.SlashCommandVisibility;
 
+import java.awt.*;
+
 /**
- * Implementation of an example command to illustrate how to respond to a user.
- * <p>
- * The implemented command is {@code /ping}, upon which the bot will respond with {@code Pong!}.
+ * The command will inform the user that the bot is alive, and it will also show the latency.
  */
 public final class PingCommand extends SlashCommandAdapter {
     /**
      * Creates an instance of the ping pong command.
      */
     public PingCommand() {
-        super("ping", "Bot responds with 'Pong!'", SlashCommandVisibility.GUILD);
+        super("ping", "Informs the user of the latency of the bot", SlashCommandVisibility.GUILD);
     }
 
     /**
-     * When triggered with {@code /ping}, the bot will respond with {@code Pong!}.
+     * When triggered with {@code /ping}, the bot will respond with the latency of the bot.
      *
      * @param event the corresponding event
      */
     @Override
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
-        event.reply("Pong!").queue();
+        long latency = event.getJDA().getGatewayPing();
+        event
+            .replyEmbeds(new EmbedBuilder().setTitle("Here is the ping:")
+                .setDescription("The WS ping is: " + latency + "ms")
+                .setColor(Color.decode("#895FE8"))
+                .build())
+            .queue();
     }
 }
