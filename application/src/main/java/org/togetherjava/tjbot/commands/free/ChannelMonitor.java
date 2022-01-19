51881210b01a19b24c7d6b83ae0acfc8db1a63ca
Zabuzard <zabuza.dev@gmail.com>
2022-01-19 18:42:47 +0100

    Added some missing curlies that made SonarLint angry

--- application/src/main/java/org/togetherjava/tjbot/commands/free/ChannelMonitor.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/free/ChannelMonitor.java
@@ -271,9 +271,10 @@ final class ChannelMonitor {
         }
         long channelId = guildIdToStatusChannel.get(guild.getIdLong());
         TextChannel channel = guild.getTextChannelById(channelId);
-        if (channel == null)
+        if (channel == null) {
             throw new IllegalStateException("Status channel %d does not exist in guild %s"
                 .formatted(channelId, guild.getName()));
+        }
         return channel;
     }
 
