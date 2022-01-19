a384cf858e24a6cda3ddb0a6be3ed7daefad0bda
Zabuzard <zabuza.dev@gmail.com>
2022-01-19 16:54:23 +0100

    Fixed some typos in user-facing strings (missing spaces)

--- application/src/main/java/org/togetherjava/tjbot/commands/free/UserStrings.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/free/UserStrings.java
@@ -23,8 +23,8 @@ enum UserStrings {
             Command not ready please try again in a minute.
             """),
     NOT_MONITORED_ERROR("This channel is not being monitored for free/busy status. If you"
-            + "believe this channel should be part of the free/busy status system, please discuss it"
-            + "with a moderator"),
+            + " believe this channel should be part of the free/busy status system, please discuss it"
+            + " with a moderator."),
     NOT_CONFIGURED_ERROR("""
             This guild (%s) is not configured to use the '/free' command.
             Please add entries in the config, restart the bot and try again.
