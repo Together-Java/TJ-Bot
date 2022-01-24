471544eb55857276b4ccd1e3286ccdd3e7f82151
JJeeff248 <52386683+jjeeff248@users.noreply.github.com>
2022-01-24 01:14:13 +0100

    Most of the work done by JJeeff, afterwards some refactoring by Tijs

---
+++ application/src/main/java/org/togetherjava/tjbot/commands/basic/RoleSelectCommand.java
@@ -0,0 +1,282 @@
+package org.togetherjava.tjbot.commands.basic;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.interaction.SelectionMenuEvent;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.Interaction;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
+import net.dv8tion.jda.api.interactions.components.selections.SelectionMenu;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.togetherjava.tjbot.commands.SlashCommandAdapter;
+import org.togetherjava.tjbot.commands.SlashCommandVisibility;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+
+/**
+ * Implements the {@code roleSelect} command.
+ *
+ * <p>
+ * Allows users to select their roles without using reactions, instead it uses selection menus where
+ * you can select multiple roles. <br />
+ * Note: the bot can only use roles below its highest one
+ */
+public class RoleSelectCommand extends SlashCommandAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(RoleSelectCommand.class);
+
+    private static final String ALL_OPTION = "all";
+    private static final String CHOOSE_OPTION = "choose";
+
+    private static final String TITLE_OPTION = "title";
+    private static final String DESCRIPTION_OPTION = "description";
+
+    private static final Color embedColor = new Color(24, 221, 136);
+
+    private static final List<OptionData> messageOptions = List.of(
+            new OptionData(OptionType.STRING, TITLE_OPTION, "The title for the message", false),
+            new OptionData(OptionType.STRING, DESCRIPTION_OPTION, "A description for the message",
+                    false));
+
+
+    /**
+     * Construct an instance
+     *
+     * @see RoleSelectCommand
+     */
+    public RoleSelectCommand() {
+        super("role-select", "Sends a message where users can select their roles",
+                SlashCommandVisibility.GUILD);
+
+        SubcommandData allRoles =
+                new SubcommandData(ALL_OPTION, "Lists all the rolls in the server for users")
+                    .addOptions(messageOptions);
+
+        SubcommandData selectRoles =
+                new SubcommandData(CHOOSE_OPTION, "Choose the roles for users to select")
+                    .addOptions(messageOptions);
+
+        getData().addSubcommands(allRoles, selectRoles);
+    }
+
+    @Override
+    public void onSlashCommand(@NotNull final SlashCommandEvent event) {
+        Member member = Objects.requireNonNull(event.getMember(), "Member is null");
+        if (!member.hasPermission(Permission.MANAGE_ROLES)) {
+            event.reply("You dont have the right permissions to use this command")
+                .setEphemeral(true)
+                .queue();
+            return;
+        }
+
+        Member selfMember = Objects.requireNonNull(event.getGuild()).getSelfMember();
+        if (!selfMember.hasPermission(Permission.MANAGE_ROLES)) {
+            event.reply("The bot needs the manage role permissions").setEphemeral(true).queue();
+            logger.warn("The bot needs the manage role permissions");
+            return;
+        }
+
+        SelectionMenu.Builder menu = SelectionMenu.create(generateComponentId(member.getId()));
+        boolean ephemeral = false;
+
+        if (Objects.equals(event.getSubcommandName(), CHOOSE_OPTION)) {
+            addMenuOptions(event, menu, "Select the roles to display", 1);
+            ephemeral = true;
+        } else {
+            addMenuOptions(event, menu, "Select your roles", 0);
+        }
+
+        // Handle Optional arguments
+        OptionMapping titleOption = event.getOption(TITLE_OPTION);
+        OptionMapping descriptionOption = event.getOption(DESCRIPTION_OPTION);
+
+        String title = handleOption(titleOption);
+        String description = handleOption(descriptionOption);
+
+        if (ephemeral) {
+            event.replyEmbeds(makeEmbed(title, description))
+                .addActionRow(menu.build())
+                .setEphemeral(true)
+                .queue();
+        } else {
+            event.getChannel()
+                .sendMessageEmbeds(makeEmbed(title, description))
+                .setActionRow(menu.build())
+                .queue();
+
+            event.reply("Message sent successfully!").setEphemeral(true).queue();
+        }
+    }
+
+    /**
+     * Adds role options to a selection menu
+     * <p>
+     *
+     * @param event the {@link SlashCommandEvent}
+     * @param menu the menu to add options to {@link SelectionMenu.Builder}
+     * @param placeHolder the placeholder for the menu {@link String}
+     * @param minValues the minimum number of selections. nullable {@link Integer}
+     */
+    private static void addMenuOptions(@NotNull final Interaction event,
+            @NotNull final SelectionMenu.Builder menu, @NotNull final String placeHolder,
+            @Nullable final Integer minValues) {
+
+
+        Guild guild = Objects.requireNonNull(event.getGuild(), "The given guild cannot be null");
+
+        Role highestBotRole = guild.getSelfMember().getRoles().get(0);
+        List<Role> guildRoles = guild.getRoles();
+
+        Collection<Role> roles = new ArrayList<>(
+                guildRoles.subList(guildRoles.indexOf(highestBotRole) + 1, guildRoles.size()));
+
+        if (null != minValues) {
+            menu.setMinValues(minValues);
+        }
+
+        menu.setPlaceholder(placeHolder).setMaxValues(roles.size());
+
+
+        menu.addOptions(roles.stream()
+            .filter(Role::isPublicRole)
+            .filter(role -> !role.getTags().isBot())
+            .map(role -> SelectOption.of(role.getName(), role.getId()))
+            .toList());
+    }
+
+    /**
+     * Creates an embedded message to send with the selection menu
+     *
+     * <p>
+     * </p>
+     *
+     * @param title for the embedded message. nullable {@link String}
+     * @param description for the embedded message. nullable {@link String}
+     * @return the formatted embed {@link MessageEmbed}
+     */
+    private static @NotNull MessageEmbed makeEmbed(@Nullable final String title,
+            @Nullable final CharSequence description) {
+
+        String effectiveTitle = title;
+
+        if (null == effectiveTitle) {
+            effectiveTitle = "Select your roles:";
+        }
+
+        return new EmbedBuilder().setTitle(effectiveTitle)
+            .setDescription(description)
+            .setColor(embedColor)
+            .build();
+    }
+
+    @Override
+    public void onSelectionMenu(@NotNull final SelectionMenuEvent event,
+            @NotNull final List<String> args) {
+        Member member = Objects.requireNonNull(event.getMember(), "Member is null");
+        Guild guild =
+                Objects.requireNonNull(event.getGuild(), "The given Guild guild cannot be null");
+        List<SelectOption> selectedOptions = Objects.requireNonNull(event.getSelectedOptions(),
+                "The given selectedOptions cannot be null");
+
+        List<Role> selectedRoles = selectedOptions.stream()
+            .map(selectOption -> guild.getRoleById(selectOption.getValue()))
+            .filter(Objects::nonNull)
+            .filter(role -> guild.getSelfMember().canInteract(role))
+            .toList();
+
+        // TODO kinda weird to check it like that?
+        // True if the event option was 'choose'
+        if (event.getMessage().isEphemeral()) {
+
+            SelectionMenu.Builder menu = SelectionMenu.create(generateComponentId(member.getId()))
+                .setPlaceholder("Select your roles")
+                .setMaxValues(selectedRoles.size())
+                .setMinValues(0);
+
+            selectedRoles.forEach(role -> menu.addOption(role.getName(), role.getId()));
+
+            event.getChannel()
+                .sendMessageEmbeds(event.getMessage().getEmbeds().get(0))
+                .setActionRow(menu.build())
+                .queue();
+
+            event.reply("Message sent successfully!").setEphemeral(true).queue();
+            return;
+        }
+
+        List<SelectOption> menuOptions =
+                Objects.requireNonNull(event.getInteraction().getComponent()).getOptions();
+
+
+        // TODO weird naming lol
+        Collection<Role> additionRoles = new ArrayList<>(selectedRoles.size());
+        Collection<Role> removalRoles = new ArrayList<>(selectedRoles.size());
+
+        menuOptions.stream().map(selectedOption -> {
+            // TODO handle in a different way? Because we need the SelectOption it has to be handled
+            // really odd in a Stream
+            Role role = guild.getRoleById(selectedOption.getValue());
+
+            if (null == role) {
+                logger.info(
+                        "The {} ({}) role has been removed but is still an option in the selection menu",
+                        selectedOption.getLabel(), selectedOption.getValue());
+                return null;
+            }
+
+            return role;
+        }).filter(Objects::nonNull).forEach(role -> {
+            if (selectedRoles.contains(role)) {
+                additionRoles.add(role);
+            } else {
+                removalRoles.add(role);
+            }
+        });
+
+        guild.modifyMemberRoles(member, additionRoles, removalRoles)
+            .flatMap(empty -> event.reply("Updated your roles!").setEphemeral(true))
+            .queue();
+
+    }
+
+    /**
+     * This gets the OptionMapping and returns the value as a string if there is one
+     *
+     * <p>
+     * </p>
+     *
+     * @param option the {@link OptionMapping}
+     * @return the value. nullable {@link String}
+     */
+    @Contract("null -> null")
+    private static @Nullable String handleOption(@Nullable final OptionMapping option) {
+        if (null == option) {
+            return null;
+        }
+
+        if (OptionType.STRING == option.getType()) {
+            return option.getAsString();
+        } else if (OptionType.BOOLEAN == option.getType()) {
+            return option.getAsBoolean() ? "true" : "false";
+        } else {
+            return null;
+        }
+    }
+}
