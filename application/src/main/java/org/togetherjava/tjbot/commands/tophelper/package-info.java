39aadbf89462aec847fc87b5e665c32ba93a57fa
d3vel0per <d3vel0per@users.noreply.github.com>
2022-01-27 12:43:41 +0100

    Added top-helpers command

---
+++ application/src/main/java/org/togetherjava/tjbot/commands/tophelper/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This packages offers all the functionality for the top-helpers command system. The core class is
+ * {@link org.togetherjava.tjbot.commands.tophelper.TopHelpersCommand}.
+ */
+package org.togetherjava.tjbot.commands.tophelper;
