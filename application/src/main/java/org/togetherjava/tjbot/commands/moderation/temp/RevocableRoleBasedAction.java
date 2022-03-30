4427e8f8b4436fb0b4beb9fb6c1f4614a22a677e
MaiTheLord <63251610+MaiTheLord@users.noreply.github.com>
2022-03-30 20:21:57 +0300

    rewrote this, without making the code worse.

---
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/temp/RevocableRoleBasedAction.java
@@ -0,0 +1,61 @@
+package org.togetherjava.tjbot.commands.moderation.temp;
+
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
+import net.dv8tion.jda.api.requests.ErrorResponse;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Role based moderation actions that can be revoked, for example a {@link TemporaryMuteAction} or a
+ * {@link TemporaryBanAction}, which are applied implicitly purely by the presence of a role.
+ */
+abstract class RevocableRoleBasedAction implements RevocableModerationAction {
+    private static final Logger logger = LoggerFactory.getLogger(RevocableRoleBasedAction.class);
+
+    private final String actionName;
+
+    /**
+     * Creates a new role based action.
+     *
+     * @param actionName the action name to be used in logging in case of a failure, e.g.
+     *        {@code "mute"}, {@code "quarantine"}
+     */
+    public RevocableRoleBasedAction(@NotNull String actionName) {
+        this.actionName = actionName;
+    }
+
+    @Override
+    public @NotNull FailureIdentification handleRevokeFailure(@NotNull Throwable failure,
+            long targetId) {
+
+        if (failure instanceof ErrorResponseException errorResponseException) {
+            if (errorResponseException.getErrorResponse() == ErrorResponse.UNKNOWN_USER) {
+                logger.debug(
+                        "Attempted to revoke a temporary {} but user '{}' does not exist anymore.",
+                        actionName, targetId);
+                return FailureIdentification.KNOWN;
+            }
+
+            if (errorResponseException.getErrorResponse() == ErrorResponse.UNKNOWN_MEMBER) {
+                logger.debug(
+                        "Attempted to revoke a temporary {} but user '{}' is not a member of the guild anymore.",
+                        actionName, targetId);
+                return FailureIdentification.KNOWN;
+            }
+
+            if (errorResponseException.getErrorResponse() == ErrorResponse.UNKNOWN_ROLE) {
+                logger.warn("Attempted to revoke a temporary {} but the {} role can not be found.",
+                        actionName, actionName);
+                return FailureIdentification.KNOWN;
+            }
+
+            if (errorResponseException.getErrorResponse() == ErrorResponse.MISSING_PERMISSIONS) {
+                logger.warn("Attempted to revoke a temporary {} but the bot lacks permission.",
+                        actionName);
+                return FailureIdentification.KNOWN;
+            }
+        }
+        return FailureIdentification.UNKNOWN;
+    }
+}
--- application/src/main/java/org/togetherjava/tjbot/commands/moderation/temp/TemporaryQuarantineAction.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/temp/TemporaryQuarantineAction.java
@@ -2,12 +2,8 @@ package org.togetherjava.tjbot.commands.moderation.temp;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.exceptions.ErrorResponseException;
-import net.dv8tion.jda.api.requests.ErrorResponse;
 import net.dv8tion.jda.api.requests.RestAction;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.togetherjava.tjbot.commands.moderation.ModerationAction;
 import org.togetherjava.tjbot.commands.moderation.ModerationUtils;
 import org.togetherjava.tjbot.config.Config;
@@ -17,8 +13,7 @@ import org.togetherjava.tjbot.config.Config;
  * {@link org.togetherjava.tjbot.commands.moderation.QuarantineCommand} and executed by
  * {@link TemporaryModerationRoutine}.
  */
-final class TemporaryQuarantineAction implements RevocableModerationAction {
-    private static final Logger logger = LoggerFactory.getLogger(TemporaryQuarantineAction.class);
+final class TemporaryQuarantineAction extends RevocableRoleBasedAction {
     private final Config config;
 
     /**
@@ -27,6 +22,8 @@ final class TemporaryQuarantineAction implements RevocableModerationAction {
      * @param config the config to use to identify the quarantined role
      */
     TemporaryQuarantineAction(@NotNull Config config) {
+        super("quarantine");
+
         this.config = config;
     }
 
@@ -47,38 +44,5 @@ final class TemporaryQuarantineAction implements RevocableModerationAction {
             .removeRoleFromMember(target.getIdLong(),
                     ModerationUtils.getQuarantinedRole(guild, config).orElseThrow())
             .reason(reason);
-    }
-
-    @Override
-    public @NotNull FailureIdentification handleRevokeFailure(@NotNull Throwable failure,
-            long targetId) {
-        if (failure instanceof ErrorResponseException errorResponseException) {
-            if (errorResponseException.getErrorResponse() == ErrorResponse.UNKNOWN_USER) {
-                logger.debug(
-                        "Attempted to revoke a temporary quarantine but user '{}' does not exist anymore.",
-                        targetId);
-                return FailureIdentification.KNOWN;
-            }
-
-            if (errorResponseException.getErrorResponse() == ErrorResponse.UNKNOWN_MEMBER) {
-                logger.debug(
-                        "Attempted to revoke a temporary quarantine but user '{}' is not a member of the guild anymore.",
-                        targetId);
-                return FailureIdentification.KNOWN;
-            }
-
-            if (errorResponseException.getErrorResponse() == ErrorResponse.UNKNOWN_ROLE) {
-                logger.warn(
-                        "Attempted to revoke a temporary quarantine but the quarantine role can not be found.");
-                return FailureIdentification.KNOWN;
-            }
-
-            if (errorResponseException.getErrorResponse() == ErrorResponse.MISSING_PERMISSIONS) {
-                logger.warn(
-                        "Attempted to revoke a temporary quarantine but the bot lacks permission.");
-                return FailureIdentification.KNOWN;
-            }
-        }
-        return FailureIdentification.UNKNOWN;
     }
 }
