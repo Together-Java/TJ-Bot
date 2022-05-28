57f4293f9f1dcf4e8cef8e7e20a94e75895434d3
Zabuzard <zabuza.dev@gmail.com>
2022-05-28 13:01:23 +0200

    Polish, improvements, bugfixes

--- application/src/main/java/org/togetherjava/tjbot/commands/basic/RoleSelectCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/basic/RoleSelectCommand.java
@@ -5,7 +5,6 @@ import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent;
-import net.dv8tion.jda.api.interactions.Interaction;
 import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
 import net.dv8tion.jda.api.interactions.commands.CommandInteraction;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
@@ -15,29 +14,36 @@ import net.dv8tion.jda.api.interactions.components.selections.SelectMenu;
 import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.togetherjava.tjbot.commands.SlashCommandAdapter;
 import org.togetherjava.tjbot.commands.SlashCommandVisibility;
+import org.togetherjava.tjbot.commands.componentids.Lifespan;
 
 import java.awt.Color;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
 /**
- * Implements the {@code roleSelect} command.
- *
+ * Implements the {@code /role-select} command.
  * <p>
- * Allows users to select their roles without using reactions, instead it uses selection menus where
- * you can select multiple roles. <br />
- * Note: the bot can only use roles with a position below its highest one
+ * The command works in two stages. First, a user sets up a role selection dialog by using the
+ * command:
+ * 
+ * <pre>
+ * {@code
+ * /role-select
+ *   title: Star Wars
+ *   description: Pick your preference
+ *   selectable-role: @Jedi
+ *   selectable-role1: @Sith
+ *   selectable-role2: @Droid
+ * }
+ * </pre>
+ * 
+ * Afterwards, users can pick their roles in a menu, upon which the command adjusts their roles.
  */
 public final class RoleSelectCommand extends SlashCommandAdapter {
 
@@ -84,23 +90,15 @@ public final class RoleSelectCommand extends SlashCommandAdapter {
             .filter(RoleSelectCommand::handleIsBotAccessibleRole)
             .toList();
 
-        if (selectedRoles.isEmpty()) {
-            // TODO Used to use the list of roles without the accessible-filter
-            event.replyEmbeds(generateLackingNonSystemRolesEmbed(rawRoles)).queue();
+        if (!handleAccessibleRolesSelected(event, selectedRoles)) {
             return;
         }
 
-        List<Role> rolesBotCantInteractWith = selectedRoles.stream()
-            .filter(role -> !event.getGuild().getSelfMember().canInteract(role))
-            .toList();
-
-        if (!rolesBotCantInteractWith.isEmpty()) {
-            event.replyEmbeds(generateCannotInteractWithRolesEmbed(rolesBotCantInteractWith))
-                .queue();
+        if (!handleInteractableRolesSelected(event, selectedRoles)) {
             return;
         }
 
-        handleCommandSuccess(event, selectedRoles);
+        sendRoleSelectionMenu(event, selectedRoles);
     }
 
     private boolean handleHasPermissions(@NotNull SlashCommandInteractionEvent event) {
@@ -124,21 +122,6 @@ public final class RoleSelectCommand extends SlashCommandAdapter {
         return true;
     }
 
-    /**
-     * Tests whether the given role is a role accessible to the bot or a system role.
-     *
-     * <p>
-     * A system role is a role where one of the following is true:
-     * <ul>
-     * <li>the {@code @everyone} role</li>
-     * <li>a bot/integration role</li>
-     * <li>the booster role</li>
-     * <li>the Twitch Subscriber role</li>
-     * </ul>
-     *
-     * @param role the role to test
-     * @return Whenever the given role is accessible to the bot, i.e. not a system role
-     */
     @Contract(pure = true)
     private static boolean handleIsBotAccessibleRole(@NotNull Role role) {
         boolean isSystemRole = role.isPublicRole() || role.getTags().isBot()
@@ -152,222 +135,143 @@ public final class RoleSelectCommand extends SlashCommandAdapter {
         return !isSystemRole;
     }
 
-    /**
-     * Collects the given {@link Collection} of {@link IMentionable IMentionables} to a comma
-     * separated String within {@code ()} <br/>
-     * It maps the {@link IMentionable IMentionables} to their mention using
-     * {@link IMentionable#getAsMention()}.
-     *
-     * @param mentionables The {@link Collection} of {@link IMentionable IMentionables} to collect
-     *        into a {@link String}
-     * @return The given mentionables their mention collected into a {@link String}
-     */
-    private static String mentionablesToJoinedString(
-            @NotNull final Collection<? extends IMentionable> mentionables) {
-        return mentionables.stream()
+    private static boolean handleAccessibleRolesSelected(@NotNull IReplyCallback event,
+            @NotNull Collection<Role> selectedRoles) {
+        if (!selectedRoles.isEmpty()) {
+            return true;
+        }
+
+        MessageEmbed embed = createEmbed("Only system roles selected",
+                """
+                        I can not interact with system roles, these roles are special roles created by Discord, such as
+                        `@everyone`, or the role given automatically to boosters.
+                        Please pick non-system roles.""");
+
+        event.replyEmbeds(embed).setEphemeral(true).queue();
+        return false;
+    }
+
+    private static boolean handleInteractableRolesSelected(@NotNull IReplyCallback event,
+            @NotNull Collection<Role> selectedRoles) {
+        List<Role> nonInteractableRoles = selectedRoles.stream()
+            .filter(role -> !event.getGuild().getSelfMember().canInteract(role))
+            .toList();
+
+        if (nonInteractableRoles.isEmpty()) {
+            return true;
+        }
+
+        String nonInteractableRolesText = nonInteractableRoles.stream()
             .map(IMentionable::getAsMention)
             .collect(Collectors.joining(", ", "(", ")"));
+
+        MessageEmbed embed = createEmbed("Lacking permission",
+                "I can not interact with %s, please contact someone to give me appropriate permissions or select other roles."
+                    .formatted(nonInteractableRolesText));
+
+        event.replyEmbeds(embed).setEphemeral(true).queue();
+        return false;
     }
 
-    /**
-     * Handles the event when all the given roles are system roles.
-     *
-     * @param systemRoles A {@link Collection} of the {@link Role roles} the bot cannot interact
-     *        with.
-     * @return A modified {@link MessageEmbed} for this error
-     */
-    private static @NotNull MessageEmbed generateLackingNonSystemRolesEmbed(
-            @NotNull final Collection<? extends Role> systemRoles) {
+    private void sendRoleSelectionMenu(@NotNull final CommandInteraction event,
+            @NotNull final Collection<? extends Role> selectableRoles) {
+        SelectMenu.Builder menu =
+                SelectMenu.create(generateComponentId(Lifespan.PERMANENT, event.getUser().getId()))
+                    .setPlaceholder("Select your roles")
+                    .setMinValues(0)
+                    .setMaxValues(selectableRoles.size());
 
-        return makeEmbed("Error: The given roles are all system roles!", """
-                The bot can't/shouldn't interact with %s, these roles are created by Discord.
-                Examples are @everyone, or the role given automatically to boosters.
-                Are you sure you picked the correct role?
-                """.formatted(mentionablesToJoinedString(systemRoles)));
+        selectableRoles.stream()
+            .map(RoleSelectCommand::mapToSelectOption)
+            .forEach(menu::addOptions);
+
+        OptionMapping titleOption = event.getOption(TITLE_OPTION);
+        String title = titleOption == null ? "Select your roles:" : titleOption.getAsString();
+
+        MessageEmbed embed = createEmbed(title, event.getOption(DESCRIPTION_OPTION).getAsString());
+
+        event.replyEmbeds(embed).addActionRow(menu.build()).queue();
     }
 
-    /**
-     * Handles the event when the bot cannot interact with certain roles.
-     *
-     * @param rolesBotCantInteractWith A {@link Collection} of the {@link Role roles} the bot cannot
-     *        interact with.
-     * @return A modified {@link MessageEmbed} for this error
-     */
-    private static @NotNull MessageEmbed generateCannotInteractWithRolesEmbed(
-            @NotNull final Collection<? extends Role> rolesBotCantInteractWith) {
-
-        return makeEmbed("Error: The role of the bot is too low!",
-                "The bot can't interact with %s, contact a staff member to move the bot above these roles."
-                    .formatted(mentionablesToJoinedString(rolesBotCantInteractWith)));
-    }
-
-    /**
-     * Creates an embed to send with the selection menu. <br>
-     * This embed is specifically designed for this command and might have unwanted side effects.
-     *
-     * @param title The title for {@link EmbedBuilder#setTitle(String)}.
-     * @param description The description for {@link EmbedBuilder#setDescription(CharSequence)}
-     * @return The formatted {@link MessageEmbed}.
-     */
-    private static @NotNull MessageEmbed makeEmbed(@Nullable final String title,
-            @Nullable final CharSequence description) {
-
-        return new EmbedBuilder().setTitle(title)
-            .setDescription(description)
-            .setColor(AMBIENT_COLOR)
-            .setTimestamp(Instant.now())
-            .build();
-    }
-
-    /**
-     * Handles the event when no issues were found and the message can be sent.
-     *
-     * @param event The {@link CommandInteraction} to reply to.
-     * @param roles A {@link List} of the {@link Role roles} that the users should be able to pick.
-     */
-    private void handleCommandSuccess(@NotNull final CommandInteraction event,
-            @NotNull final Collection<? extends Role> roles) {
-
-        SelectMenu.Builder menu = SelectMenu.create(generateComponentId(event.getUser().getId()))
-            .setPlaceholder("Select your roles")
-            .setMaxValues(roles.size())
-            .setMinValues(0);
-
-        roles.forEach(role -> menu.addOptions(mapToSelectOption(role)));
-
-        String title = null == event.getOption(TITLE_OPTION) ? "Select your roles:"
-                : event.getOption(TITLE_OPTION).getAsString();
-
-        MessageEmbed generatedEmbed =
-                makeEmbed(title, event.getOption(DESCRIPTION_OPTION).getAsString());
-
-        event.replyEmbeds(generatedEmbed).addActionRow(menu.build()).queue();
-    }
-
-    /**
-     * Maps the given role to a {@link SelectOption} with the {@link SelectOption SelectOption's}
-     * emoji, if it has one.
-     *
-     * @param role The {@link Role} to base the option from.
-     * @return The generated {@link SelectOption}.
-     */
     @NotNull
-    private static SelectOption mapToSelectOption(@NotNull final Role role) {
+    private static SelectOption mapToSelectOption(@NotNull Role role) {
         RoleIcon roleIcon = role.getIcon();
 
-        if (null == roleIcon || !roleIcon.isEmoji()) {
-            return SelectOption.of(role.getName(), role.getId());
-        } else {
-            return SelectOption.of(role.getName(), role.getId())
-                .withEmoji((Emoji.fromUnicode(roleIcon.getEmoji())));
+        SelectOption option = SelectOption.of(role.getName(), role.getId());
+        if (null != roleIcon && roleIcon.isEmoji()) {
+            option = option.withEmoji((Emoji.fromUnicode(roleIcon.getEmoji())));
         }
+
+        return option;
     }
 
-
     @Override
-    public void onSelectionMenu(@NotNull final SelectMenuInteractionEvent event,
-            @NotNull final List<String> args) {
-
-        Guild guild = Objects.requireNonNull(event.getGuild(), "The given guild cannot be null");
-        List<SelectOption> selectedOptions = Objects.requireNonNull(event.getSelectedOptions(),
-                "The given selectedOptions cannot be null");
-
-        List<Role> roles = selectedOptions.stream()
+    public void onSelectionMenu(@NotNull SelectMenuInteractionEvent event,
+            @NotNull List<String> args) {
+        Guild guild = event.getGuild();
+        List<Role> selectedRoles = event.getSelectedOptions()
+            .stream()
             .map(SelectOption::getValue)
             .map(guild::getRoleById)
             .filter(Objects::nonNull)
             .toList();
 
-        List<Role> rolesBotCantInteractWith =
-                roles.stream().filter(role -> !guild.getSelfMember().canInteract(role)).toList();
-
-        if (!rolesBotCantInteractWith.isEmpty()) {
-            event.getChannel()
-                .sendMessageEmbeds(generateCannotInteractWithRolesEmbed(rolesBotCantInteractWith))
-                .queue();
+        if (!handleInteractableRolesSelected(event, selectedRoles)) {
+            return;
         }
 
-        List<Role> usableRoles =
-                roles.stream().filter(role -> guild.getSelfMember().canInteract(role)).toList();
-
-        handleRoleSelection(event, usableRoles, guild);
+        handleRoleSelection(event, guild, selectedRoles);
     }
 
-    /**
-     * Handles selection of a {@link SelectMenuInteractionEvent}.
-     *
-     * @param event the <b>unacknowledged</b> {@link SelectMenuInteractionEvent}.
-     * @param selectedRoles The {@link Role roles} selected.
-     * @param guild The {@link Guild}.
-     */
-    private static void handleRoleSelection(final @NotNull SelectMenuInteractionEvent event,
-            final @NotNull Collection<Role> selectedRoles, final Guild guild) {
+    private static void handleRoleSelection(@NotNull SelectMenuInteractionEvent event,
+            @NotNull Guild guild, @NotNull Collection<Role> selectedRoles) {
         Collection<Role> rolesToAdd = new ArrayList<>(selectedRoles.size());
         Collection<Role> rolesToRemove = new ArrayList<>(selectedRoles.size());
 
+        // Diff the selected roles from all selectable roles
         event.getInteraction()
             .getComponent()
             .getOptions()
             .stream()
-            .map(roleFromSelectOptionFunction(guild))
-            .filter(Objects::nonNull)
-            .forEach((Role role) -> {
-                if (selectedRoles.contains(role)) {
-                    rolesToAdd.add(role);
-                } else {
-                    rolesToRemove.add(role);
-                }
+            .map(optionToRole(guild))
+            .filter(Optional::isPresent)
+            .map(Optional::orElseThrow)
+            .forEach(role -> {
+                Collection<Role> target = selectedRoles.contains(role) ? rolesToAdd : rolesToRemove;
+                target.add(role);
             });
 
-        handleRoleModifications(event, event.getMember(), guild, rolesToAdd, rolesToRemove);
+        modifyRoles(event, event.getMember(), guild, rolesToAdd, rolesToRemove);
     }
 
-    /**
-     * Creates a function that maps the {@link SelectOption} to a {@link Role} from the given
-     * {@link Guild}.
-     *
-     * @param guild The {@link Guild} to grab the roles from.
-     * @return A {@link Function} which maps {@link SelectOption} to the relating {@link Role}.
-     */
-    @Contract(pure = true)
     @NotNull
-    private static Function<SelectOption, Role> roleFromSelectOptionFunction(final Guild guild) {
-        return (SelectOption selectedOption) -> {
-            Role role = guild.getRoleById(selectedOption.getValue());
+    private static Function<SelectOption, Optional<Role>> optionToRole(@NotNull Guild guild) {
+        return option -> {
+            Role role = guild.getRoleById(option.getValue());
 
             if (null == role) {
-                logRemovedRole(selectedOption);
+                logger.info(
+                        "The {} ({}) role has been removed but is still an option in a selection menu.",
+                        option.getLabel(), option.getValue());
             }
 
-            return role;
+            return Optional.ofNullable(role);
         };
     }
 
-    /**
-     * Logs that the role of the given {@link SelectOption} doesn't exist anymore.
-     *
-     * @param selectedOption the {@link SelectOption}
-     */
-    private static void logRemovedRole(final @NotNull SelectOption selectedOption) {
-        logger.info(
-                "The {} ({}) role has been removed but is still an option in the selection menu",
-                selectedOption.getLabel(), selectedOption.getValue());
+    private static void modifyRoles(@NotNull IReplyCallback event, @NotNull Member target,
+            @NotNull Guild guild, @NotNull Collection<Role> rolesToAdd,
+            @NotNull Collection<Role> rolesToRemove) {
+        guild.modifyMemberRoles(target, rolesToAdd, rolesToRemove)
+            .flatMap(empty -> event.reply("Your roles have been updated.").setEphemeral(true))
+            .queue();
     }
 
-    /**
-     * Updates the roles of the given member.
-     *
-     * @param event an <b>unacknowledged</b> {@link Interaction} event
-     * @param member the member to update the roles of
-     * @param guild what guild to update the roles in
-     * @param additionRoles the roles to add
-     * @param removalRoles the roles to remove
-     */
-    private static void handleRoleModifications(@NotNull final IReplyCallback event,
-            final Member member, final @NotNull Guild guild, final Collection<Role> additionRoles,
-            final Collection<Role> removalRoles) {
-        guild.modifyMemberRoles(member, additionRoles, removalRoles)
-            .flatMap(empty -> event.reply("Your roles have been updated!").setEphemeral(true))
-            .queue();
+    private static @NotNull MessageEmbed createEmbed(@NotNull String title,
+            @NotNull CharSequence description) {
+        return new EmbedBuilder().setTitle(title)
+            .setDescription(description)
+            .setColor(AMBIENT_COLOR)
+            .build();
     }
 }
