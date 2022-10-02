f7b0b19aa85f81b639896900dc9a8c61cf01695e
illuminator3 <hardt-j@web.de>
2022-10-03 00:20:24 +0200

    Revert "jingle bells batman smells"
    This reverts commit b91b3a5e5c36462b2018eba731daf6dd8db92dca.

--- inmemorycompiler/src/main/java/org/togetherjava/tjbot/imc/CompilationResult.java
+++ inmemorycompiler/src/main/java/org/togetherjava/tjbot/imc/CompilationResult.java
@@ -42,4 +42,34 @@ public record CompilationResult(boolean success, byte @NotNull [] bytes,
             @NotNull Iterable<CompileInfo> compileInfos) {
         return new CompilationResult(true, bytes, compileInfos);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof CompilationResult that) || success != that.success
+                || !Arrays.equals(bytes, that.bytes)) {
+            return false;
+        }
+
+        return compileInfos.equals(that.compileInfos);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (success ? 1 : 0);
+
+        result = 31 * result + Arrays.hashCode(bytes);
+        result = 31 * result + compileInfos.hashCode();
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "CompilationResult{" + "success=" + success + ", bytes=" + Arrays.toString(bytes)
+                + ", compileInfos=" + compileInfos + '}';
+    }
 }
--- inmemorycompiler/src/main/java/org/togetherjava/tjbot/imc/CompileInfo.java
+++ inmemorycompiler/src/main/java/org/togetherjava/tjbot/imc/CompileInfo.java
@@ -11,4 +11,22 @@ import java.util.Objects;
  * @see javax.tools.Diagnostic
  */
 public record CompileInfo(@NotNull Diagnostic<?> diagnostic) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof CompileInfo that))
+            return false;
+        return diagnostic.equals(that.diagnostic);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(diagnostic);
+    }
+
+    @Override
+    public String toString() {
+        return "CompileInfo{" + "diagnostic=" + diagnostic + '}';
+    }
 }
