8dcc750c72d35efcb4f48eec2aa86f0bee7a0782
DevSerendipity <islamsakrak2002@gmail.com>
2022-10-06 17:26:27 +0200

    Adding a package.info

---
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/modmail/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This packages offers all the functionality for mod mail messages.
+ */
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package org.togetherjava.tjbot.commands.moderation.modmail;
+
+import org.togetherjava.tjbot.annotations.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
