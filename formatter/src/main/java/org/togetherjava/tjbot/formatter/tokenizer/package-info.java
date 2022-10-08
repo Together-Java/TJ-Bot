dee57841756fd1f4ea38bdbc6a048dc1f553d29b
Daniel Tischner <zabuza.dev@gmail.com>
2022-10-08 20:41:38 +0200

    Added missing package-infos (#612)

---
+++ formatter/src/main/java/org/togetherjava/tjbot/formatter/tokenizer/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Deals with tokenizing code snippets. See {@link org.togetherjava.tjbot.formatter.tokenizer.Lexer}
+ * as entry point.
+ */
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package org.togetherjava.tjbot.formatter.tokenizer;
+
+import org.togetherjava.tjbot.annotations.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ formatter/src/main/java/org/togetherjava/tjbot/formatter/util/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Contains utility used by the formatter.
+ */
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package org.togetherjava.tjbot.formatter.util;
+
+import org.togetherjava.tjbot.annotations.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
