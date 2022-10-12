8b5394d9c2f8ba7f6f3efab6de2b89a642a3cc2a
DevSerendipity <islamsakrak2002@gmail.com>
2022-10-12 09:26:22 +0200

    Manually fixing spotless errors

--- application/src/main/java/org/togetherjava/tjbot/commands/moderation/modmail/ModMailCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/moderation/modmail/ModMailCommand.java
@@ -14,6 +14,7 @@ import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.togetherjava.tjbot.commands.CommandVisibility;
 import org.togetherjava.tjbot.commands.SlashCommandAdapter;
 import org.togetherjava.tjbot.commands.utils.DiscordClientAction;
