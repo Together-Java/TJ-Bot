a5eb6dae18bff5561bb3a9a7e011917a1d8ee449
Daniel Tischner <zabuza.dev@gmail.com>
2022-10-13 10:02:23 +0200

    Forward logs to Discord - CR fixes (#607)
    * Fixed some exceptional issues from CR
    
    * fixing issue with hitting max length 6000 by extra shortening
    * sending a warning to discord when nearing the pending logs limit
    
    * typo

--- application/src/main/java/org/togetherjava/tjbot/logging/discord/DiscordLogForwarder.java
+++ application/src/main/java/org/togetherjava/tjbot/logging/discord/DiscordLogForwarder.java
@@ -39,14 +39,32 @@ final class DiscordLogForwarder {
     private static final Logger logger = LoggerFactory.getLogger(DiscordLogForwarder.class);
 
     private static final int MAX_PENDING_LOGS = 10_000;
-    private static final int MAX_BATCH_SIZE = WebhookMessage.MAX_EMBEDS;
+    private static final int MAX_PENDING_LOGS_WARNING_THRESHOLD = 8_000;
+
     private static final ScheduledExecutorService SERVICE =
             Executors.newSingleThreadScheduledExecutor();
+
+    private static final int MAX_BATCH_SIZE = WebhookMessage.MAX_EMBEDS;
     /**
-     * Has to be small enough for fitting all {@value MAX_BATCH_SIZE} embeds contained in a batch
-     * into the total character length of ~6000.
+     * The max total length of all descriptions contained in a batch of embeds sent to Discord.
+     */
+    private static final int MAX_BATCH_DESCRIPTION_TOTAL = 6_000;
+    /**
+     * Has to be small enough for fitting most {@value MAX_BATCH_SIZE} embeds contained in a batch
+     * into the total character length of {@value MAX_BATCH_DESCRIPTION_TOTAL}.
+     * <p>
+     * This limit is preferred to {@link #MAX_EMBED_DESCRIPTION_SHORT}, as usually only a few
+     * messages are too large and need to be limited.
      */
     private static final int MAX_EMBED_DESCRIPTION = 1_000;
+    /**
+     * Small enough for fitting all {@value MAX_BATCH_SIZE} embeds contained in a batch into the
+     * total character length of {@value MAX_BATCH_DESCRIPTION_TOTAL}.
+     * <p>
+     * Used when {@link #MAX_EMBED_DESCRIPTION} lead to exceeding the limit.
+     */
+    private static final int MAX_EMBED_DESCRIPTION_SHORT = 400;
+
     private static final Map<Level, Integer> LEVEL_TO_AMBIENT_COLOR =
             Map.of(Level.TRACE, 0x00B362, Level.DEBUG, 0x00A5CE, Level.INFO, 0xAC59FF, Level.WARN,
                     0xDFDF00, Level.ERROR, 0xBF2200, Level.FATAL, 0xFF8484);
@@ -84,6 +102,13 @@ final class DiscordLogForwarder {
                             Logs are forwarded to Discord slower than they pile up. Discarding the latest log...""");
             return;
         }
+        if (pendingLogs.size() >= MAX_PENDING_LOGS_WARNING_THRESHOLD) {
+            logger.warn("""
+                    Nearing the max amount of logs that can be buffered. \
+                    Logs are forwarded to Discord slower than they pile up. \
+                    Look into the issue, logs will soon be discarded otherwise...
+                    """);
+        }
 
         LogMessage log = LogMessage.ofEvent(event);
 
@@ -99,6 +124,7 @@ final class DiscordLogForwarder {
             if (logsToProcess.isEmpty()) {
                 return;
             }
+            logsToProcess = validateBatch(logsToProcess);
 
             List<WebhookEmbed> logBatch = logsToProcess.stream().map(LogMessage::embed).toList();
 
@@ -114,6 +140,21 @@ final class DiscordLogForwarder {
         synchronized (pendingLogsLock) {
             return Stream.generate(pendingLogs::remove).limit(batchSize).toList();
         }
+    }
+
+    private List<LogMessage> validateBatch(List<LogMessage> logBatch) {
+        int totalDescriptionLength = logBatch.stream()
+            .map(LogMessage::embed)
+            .map(WebhookEmbed::getDescription)
+            .mapToInt(description -> description == null ? 0 : description.length())
+            .sum();
+
+        if (totalDescriptionLength >= MAX_BATCH_DESCRIPTION_TOTAL) {
+            // Shorten logs further to go below limit
+            return logBatch.stream().map(LogMessage::shortened).toList();
+        }
+
+        return new ArrayList<>(logBatch);
     }
 
     private record LogMessage(WebhookEmbed embed,
@@ -150,6 +191,15 @@ final class DiscordLogForwarder {
             exception.printStackTrace(new PrintWriter(exceptionWriter));
 
             return logMessage + "\n" + exceptionWriter.toString().replace("\t", "> ");
+        }
+
+        private LogMessage shortened() {
+            String shortDescription =
+                    abbreviate(embed.getDescription(), MAX_EMBED_DESCRIPTION_SHORT);
+            WebhookEmbed shortEmbed =
+                    new WebhookEmbedBuilder(embed).setDescription(shortDescription).build();
+
+            return new LogMessage(shortEmbed, timestamp);
         }
 
         @Override
