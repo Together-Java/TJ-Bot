2b7ce2fcedee512f1bbe296680b5ea43c630ead8
Tanish Azad <73871477+Taz03@users.noreply.github.com>
2022-10-16 01:04:46 +0530

    Added auto-completion for tag command id option (#598)

--- application/src/main/java/org/togetherjava/tjbot/commands/tags/TagCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/tags/TagCommand.java
@@ -1,16 +1,21 @@
 package org.togetherjava.tjbot.commands.tags;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.AutoCompleteQuery;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 
 import org.togetherjava.tjbot.commands.CommandVisibility;
 import org.togetherjava.tjbot.commands.SlashCommandAdapter;
+import org.togetherjava.tjbot.commands.utils.StringDistances;
 
 import java.time.Instant;
-import java.util.Objects;
+import java.util.Collection;
 
 /**
  * Implements the {@code /tag} command which lets the bot respond content of a tag that has been
@@ -21,7 +26,7 @@ import java.util.Objects;
  */
 public final class TagCommand extends SlashCommandAdapter {
     private final TagSystem tagSystem;
-
+    private static final int MAX_SUGGESTIONS = 5;
     static final String ID_OPTION = "id";
     static final String REPLY_TO_USER_OPTION = "reply-to";
 
@@ -35,16 +40,16 @@ public final class TagCommand extends SlashCommandAdapter {
 
         this.tagSystem = tagSystem;
 
-        // TODO Think about adding an ephemeral selection menu with pagination support
-        // if the user calls this without id or similar
-        getData().addOption(OptionType.STRING, ID_OPTION, "The id of the tag to display", true)
-            .addOption(OptionType.USER, REPLY_TO_USER_OPTION,
-                    "Optionally, the user who you want to reply to", false);
+        getData().addOptions(
+                new OptionData(OptionType.STRING, ID_OPTION, "The id of the tag to display", true,
+                        true),
+                new OptionData(OptionType.USER, REPLY_TO_USER_OPTION,
+                        "Optionally, the user who you want to reply to", false));
     }
 
     @Override
     public void onSlashCommand(SlashCommandInteractionEvent event) {
-        String id = Objects.requireNonNull(event.getOption(ID_OPTION)).getAsString();
+        String id = event.getOption(ID_OPTION).getAsString();
         OptionMapping replyToUserOption = event.getOption(REPLY_TO_USER_OPTION);
 
         if (tagSystem.handleIsUnknownTag(id, event)) {
@@ -62,5 +67,23 @@ public final class TagCommand extends SlashCommandAdapter {
             message = message.setContent(replyToUserOption.getAsUser().getAsMention());
         }
         message.queue();
+    }
+
+    @Override
+    public void onAutoComplete(CommandAutoCompleteInteractionEvent event) {
+        AutoCompleteQuery focusedOption = event.getFocusedOption();
+
+        if (!focusedOption.getName().equals(ID_OPTION)) {
+            throw new IllegalArgumentException(
+                    "Unexpected option, was: " + focusedOption.getName());
+        }
+
+        Collection<Command.Choice> choices = StringDistances
+            .closeMatches(focusedOption.getValue(), tagSystem.getAllIds(), MAX_SUGGESTIONS)
+            .stream()
+            .map(id -> new Command.Choice(id, id))
+            .toList();
+
+        event.replyChoices(choices).queue();
     }
 }
