4b6ceb97ab2fe940c18ea59cfc041d19feeb4e5e
Tanish <73871477+Taz03@users.noreply.github.com>
2023-01-09 11:34:27 +0000

    removed thread deletion on user ban feature (#741)

--- application/src/main/java/org/togetherjava/tjbot/commands/Features.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/Features.java
@@ -107,7 +107,6 @@ public class Features {
         // Event receivers
         features.add(new RejoinModerationRoleListener(actionsStore, config));
         features.add(new OnGuildLeaveCloseThreadListener(database));
-        features.add(new UserBannedDeleteRecentThreadsListener(database));
         features.add(new LeftoverBookmarksListener(bookmarksSystem));
         features.add(new HelpThreadCreatedListener(helpSystemHelper));
 
