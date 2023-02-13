6ec8e0ebde0b595e32fb4ef74ed6e2d717217226
AthithyaIthayan <20B81A05D1@cvr.ac.in>
2023-02-13 05:32:26 +0530

    update the Efficiency and Simplicity

--- application/src/main/java/org/togetherjava/tjbot/features/mediaonly/MediaOnlyChannelListener.java
+++ application/src/main/java/org/togetherjava/tjbot/features/mediaonly/MediaOnlyChannelListener.java
@@ -5,8 +5,6 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.MessageType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.exceptions.ErrorHandler;
-import net.dv8tion.jda.api.requests.ErrorResponse;
 import net.dv8tion.jda.api.requests.RestAction;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
@@ -48,12 +46,11 @@ public final class MediaOnlyChannelListener extends MessageReceiverAdapter {
         }
 
         if (messageHasNoMediaAttached(message)) {
-            message.delete().flatMap(any -> dmUser(message)).queue(res -> {
-            }, new ErrorHandler().handle(ErrorResponse.CANNOT_SEND_TO_USER, err -> {
-                warnUser(message).queue((res) -> {
-                    res.delete().queueAfter(1, TimeUnit.MINUTES);
-                });
-            }));
+            message.delete().flatMap(any -> dmUser(message)).queue(any -> {
+            }, err -> {
+                pingUserAobutMedia(message)
+                    .queue(res -> res.delete().queueAfter(1, TimeUnit.MINUTES));
+            });
         }
     }
 
@@ -65,20 +62,20 @@ public final class MediaOnlyChannelListener extends MessageReceiverAdapter {
     private MessageEmbed originalMessageEmbed(Message message) {
         String originalMessageContent = message.getContentRaw();
         return new EmbedBuilder().setDescription(originalMessageContent)
-                .setColor(Color.ORANGE)
-                .build();
+            .setColor(Color.ORANGE)
+            .build();
     }
 
-    private RestAction<Message> warnUser(Message message) {
+    private RestAction<Message> pingUserAobutMedia(Message message) {
         MessageEmbed originalMessageEmbed = originalMessageEmbed(message);
 
         long authorId = message.getAuthor().getIdLong();
 
         MessageCreateData pingMessage = new MessageCreateBuilder().setContent("Hey there, you <@"
-                        + authorId
-                        + "> posted a message without media (image, video, link) in a media-only channel. Please see the description of the channel for details and then repost with media attached, thanks 😀")
-                .setEmbeds(originalMessageEmbed)
-                .build();
+                + authorId
+                + "> posted a message without media (image, video, link) in a media-only channel. Please see the description of the channel for details and then repost with media attached, thanks 😀")
+            .setEmbeds(originalMessageEmbed)
+            .build();
 
         return message.getChannel().sendMessage(pingMessage);
     }
@@ -87,12 +84,12 @@ public final class MediaOnlyChannelListener extends MessageReceiverAdapter {
         MessageEmbed originalMessageEmbed = originalMessageEmbed(message);
 
         MessageCreateData dmMessage = new MessageCreateBuilder().setContent(
-                        "Hey there, you posted a message without media (image, video, link) in a media-only channel. Please see the description of the channel for details and then repost with media attached, thanks 😀")
-                .setEmbeds(originalMessageEmbed)
-                .build();
+                "Hey there, you posted a message without media (image, video, link) in a media-only channel. Please see the description of the channel for details and then repost with media attached, thanks 😀")
+            .setEmbeds(originalMessageEmbed)
+            .build();
 
         return message.getAuthor()
-                .openPrivateChannel()
-                .flatMap(channel -> channel.sendMessage(dmMessage));
+            .openPrivateChannel()
+            .flatMap(channel -> channel.sendMessage(dmMessage));
     }
 }
