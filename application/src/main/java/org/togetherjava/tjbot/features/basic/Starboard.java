b4cae2102a1d017a6aa8cc74263b4afa80c6f376
java-coding-prodigy <java-coding-prodigy@github.com>
2023-03-10 16:51:11 +0530

    Some stuff

--- application/src/main/java/org/togetherjava/tjbot/features/basic/Starboard.java
+++ application/src/main/java/org/togetherjava/tjbot/features/basic/Starboard.java
@@ -1,13 +1,23 @@
 package org.togetherjava.tjbot.features.basic;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.internal.utils.PermissionUtil;
 import org.jetbrains.annotations.NotNull;
 
 import org.togetherjava.tjbot.config.Config;
 import org.togetherjava.tjbot.config.StarboardConfig;
 import org.togetherjava.tjbot.features.EventReceiver;
+
+import java.awt.*;
+import java.util.Optional;
 
 public class Starboard extends ListenerAdapter implements EventReceiver {
 
@@ -20,8 +30,17 @@ public class Starboard extends ListenerAdapter implements EventReceiver {
     @Override
     public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
         String emojiName = event.getReaction().getEmoji().asCustom().getName();
-        MessageChannel channel = event.getChannel();
-        // TODO
-
+        Guild guild = event.getGuild();
+        GuildChannel channel = event.getGuildChannel();
+        if (!config.getEmojiNames().contains(emojiName) || !guild.getPublicRole().hasPermission(channel, Permission.VIEW_CHANNEL)) {
+            return;
+        }
+        Optional<TextChannel> starboardChannel = guild.getTextChannelsByName(config.getStarboardChannelName(), false).stream().findFirst();
+        if (starboardChannel.isEmpty()) {
+            //TODO log?
+            return;
+        }
+        MessageEmbed embed = new EmbedBuilder().build(); //TODO build embed
+        starboardChannel.get().sendMessageEmbeds(embed).queue();
     }
 }
