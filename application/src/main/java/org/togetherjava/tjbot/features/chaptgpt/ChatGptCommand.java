11f3e8a617f2aa70cfe8ccfd97b05d1e0e0ffa09
tmcdonnell2 <tmcdonnell2@learn.athabascau.ca>
2023-04-05 09:27:17 -0700

    Requested changes to ChatGptCommand.java: - Variable name clean up - Command now only uses modal - Remove optional s from string

--- application/src/main/java/org/togetherjava/tjbot/features/chaptgpt/ChatGptCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/features/chaptgpt/ChatGptCommand.java
@@ -4,7 +4,6 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.components.Modal;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
@@ -23,9 +22,8 @@ import java.util.Optional;
  * which it will respond with an AI generated answer.
  */
 public final class ChatGptCommand extends SlashCommandAdapter {
-    private static final String QUESTION_OPTION = "question";
-    private static final String MESSAGE_INPUT = "message";
-    private static final int MESSAGE_INPUT_LENGTH = 200;
+    private static final String QUESTION_INPUT = "question";
+    private static final int MAX_MESSAGE_INPUT_LENGTH = 200;
     private static final int MIN_MESSAGE_INPUT_LENGTH = 4;
     private static final Duration CACHE_DURATION = Duration.of(10, ChronoUnit.SECONDS);
     private final ChatGptService chatGptService;
@@ -42,8 +40,6 @@ public final class ChatGptCommand extends SlashCommandAdapter {
         super("chatgpt", "Ask the ChatGPT AI a question!", CommandVisibility.GUILD);
 
         this.chatGptService = chatGptService;
-
-        getData().addOption(OptionType.STRING, QUESTION_OPTION, "What do you want to ask?", false);
     }
 
     @Override
@@ -52,14 +48,10 @@ public final class ChatGptCommand extends SlashCommandAdapter {
         if (previousAskTime != null) {
             long timeRemainingUntilNextAsk = CACHE_DURATION.getSeconds()
                     - Duration.between(previousAskTime, Instant.now()).get(ChronoUnit.SECONDS);
-            String s = "";
-            if (timeRemainingUntilNextAsk > 1) {
-                s = "s";
-            }
 
             event
-                .reply("Sorry, you need to wait another " + timeRemainingUntilNextAsk + " second"
-                        + s + " before asking chatGPT another question.")
+                .reply("Sorry, you need to wait another " + timeRemainingUntilNextAsk
+                        + " second(s) before asking chatGPT another question.")
                 .setEphemeral(true)
                 .queue();
             return;
@@ -67,43 +59,23 @@ public final class ChatGptCommand extends SlashCommandAdapter {
 
         if (event.getInteraction().getOptions().isEmpty()) {
             TextInput body = TextInput
-                .create(MESSAGE_INPUT, "Ask ChatGPT a question or get help with code",
+                .create(QUESTION_INPUT, "Ask ChatGPT a question or get help with code",
                         TextInputStyle.PARAGRAPH)
                 .setPlaceholder("Put your question for ChatGPT here")
-                .setRequiredRange(MIN_MESSAGE_INPUT_LENGTH, MESSAGE_INPUT_LENGTH)
+                .setRequiredRange(MIN_MESSAGE_INPUT_LENGTH, MAX_MESSAGE_INPUT_LENGTH)
                 .build();
 
             Modal modal = Modal.create(generateComponentId(), "ChatGPT").addActionRow(body).build();
             event.replyModal(modal).queue();
-            return;
         }
-
-        if (event.getOption(QUESTION_OPTION).getAsString().length() > MESSAGE_INPUT_LENGTH) {
-            event.getHook()
-                .sendMessage("Questions to ChatGPT must be less than " + MESSAGE_INPUT_LENGTH
-                        + " characters in length. Please rephrase and try again.")
-                .queue();
-            return;
-        }
-
-        event.deferReply().queue();
-
-        Optional<String> optional =
-                chatGptService.ask(event.getOption(QUESTION_OPTION).getAsString());
-        if (optional.isPresent()) {
-            userIdToAskedAtCache.put(event.getMember().getId(), Instant.now());
-        }
-
-        String response = optional.orElse(
-                "An error has occurred while trying to communicate with ChatGPT. Please try again later");
-        event.getHook().sendMessage(response).queue();
     }
 
     @Override
     public void onModalSubmitted(ModalInteractionEvent event, List<String> args) {
         event.deferReply().queue();
 
-        Optional<String> optional = chatGptService.ask(event.getValue(MESSAGE_INPUT).getAsString());
+        Optional<String> optional =
+                chatGptService.ask(event.getValue(QUESTION_INPUT).getAsString());
         if (optional.isPresent()) {
             userIdToAskedAtCache.put(event.getMember().getId(), Instant.now());
         }
