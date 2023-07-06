63fb7f9ecd469cf5a7ea4886c45006c5734e40a4
tmcdonnell2 <t.w.mcdonnell10@gmail.com>
2023-07-05 22:01:38 -0700

    - Add ChatGptAPITest (rename ChatGptTest) - Add ChatGptServiceTest.java for testing AI responses of different length - Add response.txt which contains test AI responses (just one for now). - Refactor ChatGptService to not include response parsing/break up. Moved to new util class AIResponseParser

--- application/src/test/java/org/togetherjava/tjbot/features/chatgpt/ChatGptServiceTest.java
+++ application/src/test/java/org/togetherjava/tjbot/features/chatgpt/ChatGptServiceTest.java
@@ -1,144 +1,56 @@
 package org.togetherjava.tjbot.features.chatgpt;
 
+import com.theokanning.openai.completion.chat.ChatCompletionChoice;
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import com.theokanning.openai.completion.chat.ChatMessage;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.togetherjava.tjbot.config.Config;
-import org.togetherjava.tjbot.features.chaptgpt.ChatGptService;
+import org.togetherjava.tjbot.features.chaptgpt.AIResponseParser;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+class AIResponseParserTest {
+    private static final Logger logger = LoggerFactory.getLogger(AIResponseParserTest.class);
+    private AIResponseParser aiResponseParser;
 
-class ChatGptServiceTest {
-    private static final Logger logger = LoggerFactory.getLogger(ChatGptServiceTest.class);
-    private Config config;
-    private ChatGptService chatGptService;
+    @ParameterizedTest
+    @CsvFileSource(resources = "/AITestResponses/responses")
+    void correctResponseLength(File file) {
+        try {
+            BufferedReader input =
+                    new BufferedReader(new InputStreamReader(Files.newInputStream(file.toPath())));
+            Optional<String[]> aiResponse = AIResponseParser.parse(input.lines().toString());
 
-    @BeforeEach
-    void setUp() {
-        config = mock();
-        String openaiKey = System.getenv("openai-key");
-        when(config.getOpenaiApiKey()).thenReturn(openaiKey);
-        chatGptService = new ChatGptService(config);
+            aiResponse.ifPresentOrElse((responses) -> {
+                testResponseLength(responses);
+                toLog(responses);
+            }, Assertions.fail());
+        } catch (IOException ex) {
+            logger.error("{}", ex.getMessage());
+            Assertions.fail();
+        }
     }
 
-    @Test
-    void askToGenerateLongPoem() {
-        Optional<String[]> response = chatGptService.ask("generate a long poem");
-        response.ifPresent(this::toLog);
-        response.ifPresent(this::testResponseLength);
-    }
+    private ChatCompletionResult createChatCompletionResult(String message) {
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setContent(message);
 
-    @Test
-    void askHowToSetupJacksonLibraryWithExamples() {
-        Optional<String[]> response =
-                chatGptService.ask("How to setup jackson library with examples");
-        response.ifPresent(this::toLog);
-        response.ifPresent(this::testResponseLength);
-    }
+        ChatCompletionChoice chatCompletionChoice = new ChatCompletionChoice();
+        chatCompletionChoice.setMessage(chatMessage);
 
-    @Test
-    void askDockerReverseProxyWithNginxGuide() {
-        Optional<String[]> response = chatGptService.ask("Docker reverse proxy with nginx guide");
-        response.ifPresent(this::toLog);
-        response.ifPresent(this::testResponseLength);
-    }
-
-    @Test
-    void askWhatIsWrongWithMyCode() {
-        Optional<String[]> response = chatGptService.ask("""
-                    What is wrong with my code?
-
-                    package Lab13;
-                    import java.lang.reflect.Array;
-                    import java.util.*;
-                    import java.io.*;
-
-
-                    public class RandomDoubleFile {
-
-                        Random r = new Random();
-                        //Read File
-
-
-                        public void createDouble() {
-
-                            try {
-                                //Create/Open/Write File
-                                FileOutputStream f = new FileOutputStream("double.dat");
-                                BufferedOutputStream b = new BufferedOutputStream(f);
-                                DataOutputStream d = new DataOutputStream(b);
-
-                                //Write Number of Doubles
-                                int myint = r.nextInt(50);
-                                d.write(myint);
-
-                                //Write Doubles
-                                for (int i = 0; i < myint ; i++) {
-                                    d.writeDouble(r.nextDouble());
-                                }
-                                d.close();
-                            }catch (IOException e) {
-                                System.out.println(e.getMessage());
-                            }
-
-                        }
-                        public void readDouble() {
-
-                            double[] doublesArray = new double[0];
-
-                            try {
-                                //Configure Read Streams
-                                FileInputStream f = new FileInputStream("double.dat");
-                                BufferedInputStream b = new BufferedInputStream(f);
-                                DataInputStream d = new DataInputStream(b);
-
-                                //Read Num of Doubles
-                                int numberOfDoubles = d.readInt();
-                                doublesArray = new double[numberOfDoubles];
-
-                                //Try-Catch End-of-file
-                                try {
-                                    //Store Doubles in List
-                                    for (int i = 0; ; i++) {
-                                        doublesArray[i] = d.readDouble();
-                                    }
-
-                                } catch (EOFException eof) {
-                                    d.close();
-                                }
-                            }catch (IOException e) {
-                                System.out.println(e.getMessage());
-                            }
-
-                            //ReadList
-                            for(int i=0;i < Array.getLength(doublesArray); i++){
-                            System.out.println(doublesArray[i]);
-                            }
-                        }
-
-                            public static void main(String[] args){
-                                RandomDoubleFile rdb = new RandomDoubleFile();
-                                //rdb.createDouble();
-                                rdb.readDouble();
-                            }
-                    }
-                """);
-        response.ifPresent(this::toLog);
-        response.ifPresent(this::testResponseLength);
-    }
-
-    @Test
-    void askWhyDoesItTakeYouMoreThan10SecondsToAnswer() {
-        Optional<String[]> response =
-                chatGptService.ask("Why does it take you more than 10 seconds to answer");
-        response.ifPresent(this::toLog);
-        response.ifPresent(this::testResponseLength);
+        ChatCompletionResult chatCompletionResult = new ChatCompletionResult();
+        chatCompletionResult.setChoices(List.of(chatCompletionChoice));
+        return chatCompletionResult;
     }
 
     private void testResponseLength(String[] responses) {
