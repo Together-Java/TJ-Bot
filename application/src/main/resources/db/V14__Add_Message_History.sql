1c12b9d7a9df56efba855bc056f9f49c8151ef96
alphaBEE <ankitsmt211@gmail.com>
2023-11-06 03:52:18 +0530

    new table to hold message history

---
+++ application/src/main/resources/db/V14__Add_Message_History.sql
@@ -0,0 +1,9 @@
+CREATE TABLE message_history
+(
+    id           INTEGER   NOT NULL PRIMARY KEY AUTOINCREMENT,
+    sent_at      TIMESTAMP NOT NULL,
+    guild_id     BIGINT    NOT NULL,
+    channel_id   BIGINT    NOT NULL,
+    message_id   BIGINT    NOT NULL,
+    author_id    BIGINT    NOT NULL,
+)
