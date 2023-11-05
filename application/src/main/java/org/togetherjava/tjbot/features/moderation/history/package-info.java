d36832465975f4c781bfe7611d36ed1cf9db7a57
alphaBEE <ankitsmt211@gmail.com>
2023-11-06 03:54:46 +0530

    add package for purge command feature

---
+++ application/src/main/java/org/togetherjava/tjbot/features/moderation/history/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package offers moderation command to delete messages from a user within last hour.
+ */
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package org.togetherjava.tjbot.features.moderation.history;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import org.togetherjava.tjbot.annotations.MethodsReturnNonnullByDefault;
