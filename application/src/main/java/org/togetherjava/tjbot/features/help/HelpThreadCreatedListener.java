0a9eb9643199ceed4db96277814c82d427d3366c
alphaBEE <ankitsmt211@gmail.com>
2023-11-14 10:25:41 +0530

    refactor dismiss gpt ans button to allow embed author use it

--- application/src/main/java/org/togetherjava/tjbot/features/help/HelpThreadCreatedListener.java
+++ application/src/main/java/org/togetherjava/tjbot/features/help/HelpThreadCreatedListener.java
@@ -159,8 +159,11 @@ public final class HelpThreadCreatedListener extends ListenerAdapter
         // This method handles chatgpt's automatic response "dismiss" button
         ThreadChannel channel = event.getChannel().asThreadChannel();
         Member interactionUser = Objects.requireNonNull(event.getMember());
+        Message forumPostMessage = channel.retrieveMessageById(channel.getId()).complete();
+
+        boolean isEmbedAuthor = isPostAuthor(interactionUser, forumPostMessage);
         if (channel.getOwnerIdLong() != interactionUser.getIdLong()
-                && !helper.hasTagManageRole(interactionUser)) {
+                && !helper.hasTagManageRole(interactionUser) && (!isEmbedAuthor)) {
             event.reply("You do not have permission for this action.").setEphemeral(true).queue();
             return;
         }
@@ -182,4 +185,13 @@ public final class HelpThreadCreatedListener extends ListenerAdapter
         throw new UnsupportedOperationException("Not used");
     }
 
+    private boolean isPostAuthor(Member user, Message message) {
+        if (message.getEmbeds().isEmpty())
+            return false;
+        String embedAuthor = Objects
+            .requireNonNull(message.getEmbeds().get(0).getAuthor(),
+                    "embed author for forum post is null")
+            .getName();
+        return embedAuthor.equals(user.getUser().getName());
+    }
 }
