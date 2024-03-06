dc3382a71b4a385cd5d95740265079e5a73c09c2
christolis <work@chris-sdogkos.com>
2024-03-07 00:24:28 +0200

    feat: add CakeDayConfig record constructor

--- application/src/main/java/org/togetherjava/tjbot/config/CakeDayConfig.java
+++ application/src/main/java/org/togetherjava/tjbot/config/CakeDayConfig.java
@@ -2,9 +2,18 @@ package org.togetherjava.tjbot.config;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Objects;
+
 /**
  * Configuration record for the Cake Day feature.
  */
 public record CakeDayConfig(
         @JsonProperty(value = "rolePattern", required = true) String rolePattern) {
+
+    /**
+     * Configuration constructor for the Cake Day feature.
+     */
+    public CakeDayConfig {
+        Objects.requireNonNull(rolePattern);
+    }
 }
