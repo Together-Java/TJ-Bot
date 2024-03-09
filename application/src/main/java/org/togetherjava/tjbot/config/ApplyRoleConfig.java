083058da0cffbd2a8d3f952def9d31faa2ec9eae
christolis <work@chris-sdogkos.com>
2024-03-09 02:29:31 +0200

    style: add JavaDocs where necessary

--- application/src/main/java/org/togetherjava/tjbot/config/ApplyRoleConfig.java
+++ application/src/main/java/org/togetherjava/tjbot/config/ApplyRoleConfig.java
@@ -4,10 +4,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Objects;
 
+/**
+ * Represents the configuration for applying a role.
+ */
 public record ApplyRoleConfig(@JsonProperty(value = "name", required = true) String name,
         @JsonProperty(value = "description", required = true) String description,
         @JsonProperty(value = "formattedEmoji") String emoji) {
 
+    /**
+     * Constructs an instance of ApplyRoleConfig with the given parameters.
+     *
+     * @param name the name of the role
+     * @param description the description of the role
+     * @param emoji the emoji associated with the role
+     */
     public ApplyRoleConfig {
         Objects.requireNonNull(name);
         Objects.requireNonNull(description);
