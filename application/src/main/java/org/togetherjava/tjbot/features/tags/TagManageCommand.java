82010bd5601059ef1e70c0ff8317b82207d37c11
Tanish Azad <tanishazad03@gmail.com>
2024-05-24 02:02:24 +0530

    fix spotless error by pinning the version

--- application/src/main/java/org/togetherjava/tjbot/features/tags/TagManageCommand.java
+++ application/src/main/java/org/togetherjava/tjbot/features/tags/TagManageCommand.java
@@ -318,27 +318,18 @@ public final class TagManageCommand extends SlashCommandAdapter {
      * @param event the event to send messages with
      * @return whether the status of the given tag is <b>not equal</b> to the required status
      */
-    // ToDo: gradle task :application:spotlessJava throws internal exception if this method uses new
-    // when keyword
-    @SuppressWarnings("java:S6916")
     private boolean isWrongTagStatusAndHandle(TagStatus requiredTagStatus, String id,
             IReplyCallback event) {
-        switch (requiredTagStatus) {
-            case TagStatus.EXISTS -> {
-                return tagSystem.handleIsUnknownTag(id, event);
+        return switch (requiredTagStatus) {
+            case TagStatus.EXISTS -> tagSystem.handleIsUnknownTag(id, event);
+            case TagStatus status when status == TagStatus.NOT_EXISTS && tagSystem.hasTag(id) -> {
+                event.reply("The tag with id '%s' already exists.".formatted(id))
+                    .setEphemeral(true)
+                    .queue();
+                yield true;
             }
-            case TagStatus.NOT_EXISTS -> {
-                if (tagSystem.hasTag(id)) {
-                    event.reply("The tag with id '%s' already exists.".formatted(id))
-                        .setEphemeral(true)
-                        .queue();
-                    return true;
-                }
-            }
-            default ->
-                throw new AssertionError("Unknown tag status '%s'".formatted(requiredTagStatus));
-        }
-        return false;
+            case TagStatus.NOT_EXISTS -> false;
+        };
     }
 
     private void logAction(Subcommand subcommand, Guild guild, User author,
