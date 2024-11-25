5f84403175673899506b6b3650c3ccb7ac2e99c7
Zabuzard <zabuza.dev@gmail.com>
2024-11-25 10:15:11 +0100

    Revert "Removed now obsolete HelperPrune routine (config change) (#1076)"
    This reverts commit a11088e7e5aa65b519edb3ee966d14164223935c.

--- application/src/main/java/org/togetherjava/tjbot/features/Features.java
+++ application/src/main/java/org/togetherjava/tjbot/features/Features.java
@@ -132,6 +132,8 @@ public class Features {
         features.add(new ScamHistoryPurgeRoutine(scamHistoryStore));
         features.add(new HelpThreadMetadataPurger(database));
         features.add(new HelpThreadActivityUpdater(helpSystemHelper));
+        features
+            .add(new AutoPruneHelperRoutine(config, helpSystemHelper, modAuditLogWriter, database));
         features.add(new HelpThreadAutoArchiver(helpSystemHelper));
         features.add(new LeftoverBookmarksCleanupRoutine(bookmarksSystem));
         features.add(new MarkHelpThreadCloseInDBRoutine(database, helpThreadLifecycleListener));
