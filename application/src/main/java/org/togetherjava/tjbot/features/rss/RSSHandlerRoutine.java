338e09d00ec44566b394e7871e7ffbe141198893
Nathan <196140351+pot@users.noreply.github.com>
2025-06-29 14:44:43 -0600

    Update RSSHandlerRoutine.java

--- application/src/main/java/org/togetherjava/tjbot/features/rss/RSSHandlerRoutine.java
+++ application/src/main/java/org/togetherjava/tjbot/features/rss/RSSHandlerRoutine.java
@@ -391,7 +391,7 @@ public final class RSSHandlerRoutine implements Routine {
         try {
             return rssReader.read(rssUrl).toList();
         } catch (IOException e) {
-            logger.warn("Could not fetch RSS from URL ({})", rssUrl);
+            logger.error("Could not fetch RSS from URL ({})", rssUrl, e;
             return List.of();
         }
     }
