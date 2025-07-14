ee56ef9f1877fa36867e1ed88f769c3cb9da47cc
tferdous17 <tferdous611@gmail.com>
2025-07-14 19:52:44 -0400

    implement function to check if a link is dead or not

--- application/src/main/java/org/togetherjava/tjbot/features/utils/LinkDetection.java
+++ application/src/main/java/org/togetherjava/tjbot/features/utils/LinkDetection.java
@@ -3,15 +3,26 @@ package org.togetherjava.tjbot.features.utils;
 import com.linkedin.urls.Url;
 import com.linkedin.urls.detection.UrlDetector;
 import com.linkedin.urls.detection.UrlDetectorOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Utility class to detect links.
  */
 public class LinkDetection {
+
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private static final Logger logger = LoggerFactory.getLogger(LinkDetection.class);
 
     /**
      * Possible ways to filter a link.
@@ -80,4 +91,37 @@ public class LinkDetection {
         return Optional.of(link);
     }
 
+    public static CompletableFuture<Boolean> isLinkDead(String url) {
+        if (url == null || url.trim().isEmpty()) {
+            // Treat null/empty links as dead
+            return CompletableFuture.completedFuture(true);
+        }
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder().uri(new URI(url)).GET().build();
+
+            CompletableFuture<Boolean> linkStatusFuture =
+                    client.sendAsync(request, HttpResponse.BodyHandlers.discarding())
+                        .thenApply(HttpResponse::statusCode)
+                        .thenApply(statusCode -> {
+                            // Only links between 2xx-3xx are considered valid, therefore not dead.
+                            if ((statusCode >= 200 && statusCode <= 299)
+                                    || (statusCode >= 300 && statusCode <= 399)) {
+                                return false;
+                            }
+                            return true;
+                        })
+                        .exceptionally(throwable -> {
+                            logger.error(
+                                    "Error validating URL " + url + ": " + throwable.getMessage());
+                            return true;
+                        });
+
+            return linkStatusFuture;
+
+        } catch (URISyntaxException e) {
+            logger.error("Invalid URL " + url + ": " + e.getMessage());
+            return CompletableFuture.completedFuture(true);
+        }
+    }
 }
