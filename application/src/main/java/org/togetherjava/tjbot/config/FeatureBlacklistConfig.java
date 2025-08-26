6f64695270d88b051101f314a562dbb372d54eee
Daniel Tischner <zabuza.dev@gmail.com>
2025-08-26 11:33:36 +0200

    Proper doc for FeatureBlacklist (#1309)

--- application/src/main/java/org/togetherjava/tjbot/config/FeatureBlacklistConfig.java
+++ application/src/main/java/org/togetherjava/tjbot/config/FeatureBlacklistConfig.java
@@ -6,7 +6,25 @@ import java.util.Objects;
 
 /**
  * Configuration of the feature blacklist, any feature present here will be disabled.
+ * <p>
+ * The argument {@code "normal"} expects a list of fully qualified class names of
+ * {@link org.togetherjava.tjbot.features.Feature}s, for example:
  * 
+ * <pre>
+ * {@code
+ * "normal": [
+ *   "org.togetherjava.tjbot.features.basic.PingCommand",
+ *   "org.togetherjava.tjbot.features.tophelper.TopHelpersAssignmentRoutine"
+ * ]}
+ * </pre>
+ * <p>
+ * The argument {@code "special"} is a special set of predefined strings that disable specific
+ * features. Currently available are:
+ * <ul>
+ * <li>{@code "org.togetherjava.tjbot.features.code.FormatCodeCommand"}</li>
+ * <li>{@code "org.togetherjava.tjbot.features.code.EvalCodeCommand"}</li>
+ * </ul>
+ *
  * @param normal the normal features, which are present in
  *        {@link org.togetherjava.tjbot.features.Features}
  * @param special the special features, which require special code
