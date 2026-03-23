3f2ef465edf3f3e10e12ffb03e83fefbab39bf88
Suraj Kumar <sk96.uk@gmail.com>
2026-03-23 21:09:14 +0000

    feat: create migration for the offending metrics to use dimensions

--- application/src/main/resources/db/V17__Add_Metric_Dimensions.sql
+++ application/src/main/resources/db/V17__Add_Metric_Dimensions.sql
@@ -1,1 +1,26 @@
 ALTER TABLE metric_events ADD COLUMN dimensions TEXT;
+
+UPDATE metric_events
+SET event = 'code_action',
+    dimensions = json_object('name', SUBSTR(event, LENGTH('code_action-') + 1))
+WHERE event LIKE 'code_action-%';
+
+UPDATE metric_events
+SET event = 'autoprune_helper',
+    dimensions = json_object('role', SUBSTR(event, LENGTH('autoprune_helper-') + 1))
+WHERE event LIKE 'autoprune_helper-%';
+
+UPDATE metric_events
+SET event = 'help-category',
+    dimensions = json_object('category', SUBSTR(event, LENGTH('help-category-') + 1))
+WHERE event LIKE 'help-category-%';
+
+UPDATE metric_events
+SET event = 'tag',
+    dimensions = json_object('id', SUBSTR(event, LENGTH('tag-') + 1))
+WHERE event LIKE 'tag-%';
+
+UPDATE metric_events
+SET event = 'top_helper',
+    dimensions = json_object('userId', CAST(SUBSTR(event, LENGTH('top_helper-') + 1) AS INTEGER))
+WHERE event LIKE 'top_helper-%';
